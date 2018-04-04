7ea6fcf4332a4ec3a7142833251b6dd43239141b
王瑜琪 <greenfish84@126.com>
2018-04-04 13:34:52 +0800

    sagas处理了一下，可以支持多个action

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -12,22 +12,32 @@ import {
   GET_POSTS_URL
 } from '../../constants/url';
 
+const FetchActions = [GET_POSTS_SAGA];
+
 // worker saga
 function* showPostsAsync(action) {
+  let type = action.type;
+  console.log('start action:' + type)
   try {
+    console.log('=========Action RUN================')
     const response = yield call(axios.get, GET_POSTS_URL);
     yield put({ type: GET_POSTS_SUCCESS, posts: response.data });
+    console.log('=========Dispatch Success================');
   } catch (e) {
     yield put({ type: GET_POSTS_FAIL, error: e });
+    console.log('=========Dispatch Failure================');
+    console.log(error);
   }
 }
 
 // watcher saga
 function* watchGetPosts() {
-  yield takeLatest(GET_POSTS_SAGA, showPostsAsync);
+  yield takeLatest(FetchActions, showPostsAsync);
 }
 
 // root saga
 export default function* rootSaga() {
-  yield watchGetPosts()
+  yield [
+    watchGetPosts()
+  ]
 } 
