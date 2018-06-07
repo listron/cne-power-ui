c1f78a806dfe8043fce23aa811a2958d9cb91907
王瑜琪 <greenfish84@126.com>
2018-06-07 21:24:23 +0800

    添加工单切换

---
+++ src/containers/Ticket/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {Tabs} from 'antd';
+import Bug from './Bug';
+import Inspection from './Inspection';
+const TabPane = Tabs.TabPane;
+
+
+class Ticket extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: "bug"
+    };
+    this.onChangeTab = this.onChangeTab.bind(this);
+  }
+
+  onChangeTab(tab) {
+    this.setState({
+      tab: tab
+    });
+  }
+
+  render() {
+    return (
+      <div style={{display:"flex", flex: 1}}>
+        <Tabs activeKey={this.state.tab} onChange={this.onChangeTab}>
+          <TabPane tab="缺陷" key="bug"><Bug /></TabPane>
+          <TabPane tab="巡检" key="inspection"><Inspection /></TabPane>
+        </Tabs>
+      </div>
+    );
+  }
+}
+
+export default Ticket;
