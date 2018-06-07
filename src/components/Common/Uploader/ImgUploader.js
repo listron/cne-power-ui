4bcc40274b40c0fc8aa6db84520705d3fd7c2a06
zwy <18301417091@163.com>
2018-06-07 19:34:13 +0800

    上传按钮调整

--- src/components/Common/Uploader/ImgUploader.js
+++ src/components/Common/Uploader/ImgUploader.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import { Form, Input, Button, Row, Col } from 'antd';
+import { Upload, Modal, Icon } from 'antd';
 import PropTypes from 'prop-types';
 
 class ImgUploader extends Component {
   static propTypes = {
-    code: PropTypes.object,
-    phone: PropTypes.object,
-    history:PropTypes.array,
-    form:PropTypes.func,
-    error: PropTypes.string,
-    isFetching: PropTypes.bool,
-    changePSW:PropTypes.func
+    max: PropTypes.number,//max 图片数量
+    limitSize: PropTypes.number,//图片大小限制。
+    editable : PropTypes.bool, //是否可编辑组件
+    value: PropTypes.array, //现有文件信息列表
+    onChange: PropTypes.func, //输出上传插件信息
   }
   constructor(props) {
     super(props);
+    this.state = {
+      previewVisible: false,
+      previewImage: '',
+      fileList: [{
+        uid: -1,
+        name: 'xxx.png',
+        status: 'done',
+        url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+      }],
+    };
   }
+  handleCancel = () => this.setState({ previewVisible: false })
+  handlePreview = (file) => {
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true,
+    });
+  }
+  handleChange = ({ fileList }) => this.setState({ fileList })
 
   render() {
+    const { previewVisible, previewImage, fileList } = this.state;
+    const uploadButton = (
+      <div>
+        <Icon type="plus" />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
     return (
-      <div>图片上传插件</div>
+      <div className="clearfix">
+        <Upload
+          action="//jsonplaceholder.typicode.com/posts/"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={this.handlePreview}
+          onChange={this.handleChange}
+        >
+          {fileList.length >= 3 ? null : uploadButton}
+        </Upload>
+        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+          <img alt="example" style={{ width: '100%' }} src={previewImage} />
+        </Modal>
+      </div>
     )
+    
   }
 }
 export default ImgUploader;
