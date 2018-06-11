638d5c1d6bbb1ecace188e375bd8d1a9b6f6c116
王瑜琪 <greenfish84@126.com>
2018-06-11 17:24:36 +0800

    添加电站设备选择组件

--- src/components/Common/SelectItem/index.js
+++ src/components/Common/SelectItem/index.js
@@ -20,22 +20,37 @@ class SelectedItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+    };
+    this.onSelectItem = this.onSelectItem.bind(this);
     
   }
 
+  onSelectItem() {
+    this.props.onSelect(this.props.label);
+  }
+
+  onSelectItem() {
+    this.props.onSelect(this.props.label, !this.props.selected);
+  }
 
   render() {
     if(this.props.selected) {
       return (
-        <div className={styles.selectedItem} style={{height: this.props.height, width: this.props.width}}>
+        <div 
+          className={styles.selectedItem} 
+          style={{height: this.props.height, width: this.props.width}}
+        >
           <div className={styles.itemLabel}>{this.props.label}</div>
           <Icon type="check-circle-o" />
         </div>
       )
     } else {
       return ( 
-        <div className={styles.unSelectedItem} style={{height: this.props.height, width: this.props.width}}>
+        <div 
+          className={styles.unSelectedItem} 
+          style={{height: this.props.height, width: this.props.width}}
+          onClick={this.onSelectItem}
+        >
           <div className={styles.itemLabel}>{this.props.label}</div>
         </div>
       );
