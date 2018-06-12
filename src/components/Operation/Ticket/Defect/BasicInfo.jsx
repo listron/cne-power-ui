bf766fc49e7be2219a51b4ee6547c8e0e0537c2b
王瑜琪 <greenfish84@126.com>
2018-06-12 21:27:25 +0800

    缺陷详情基本信息

--- src/components/Operation/Ticket/Defect/BasicInfo.jsx
+++ src/components/Operation/Ticket/Defect/BasicInfo.jsx
@@ -2,7 +2,7 @@ import React,{ Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './basicInfo.scss';
 import {Card} from 'antd';
-import {getLevel, getStatus} from '../../../../constants/ticket';
+import {getLevel} from '../../../../constants/ticket';
 import ImgUploader from '../../../Common/Uploader/ImgUploader'
 
 class BasicInfo extends Component {
@@ -31,11 +31,31 @@ class BasicInfo extends Component {
         <div>缺陷描述<span>{info.get("defectDescribe")}</span></div>
         <div>查看照片
           <div>
-
+            <ImgUploader editable={false} value={info.get("images")} />
           </div>
         </div>
       </div>
-    )
+    );
+  }
+
+  renderDeal() {
+    let dealData = this.props.basicInfo.get("handleData");
+    return (
+      <div>
+        <div>
+          {dealData.get("status") === 1 ? "处理建议" : "审核意见"}
+          <span>{dealData.get("defectProposal")}</span>
+          </div>
+        {dealData.get("status") === 3 &&
+          <div>处理结果
+            <span>{dealData.get("defectSolveResult") === 0 ?"已解决":"未解决"}</span>
+          </div>}
+        {dealData.get("status") === 3 &&
+          <div>处理过程<span>{dealData.get("defectSolveInfo")}</span></div>}
+        {dealData.get("status") === 3 && dealData.get("replaceParts") !== "" &&
+          <div>更换备件<span>{dealData.get("replaceParts")}</span></div>}
+      </div>
+    );
   }
 
   render() {   
