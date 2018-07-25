0ee2a7e9a23bade1d2bc22b168712c312763f6aa
tiandali <tian.chengli@outlook.com>
2018-07-25 20:25:07 +0800

    登陆注册页面布局更改

--- src/containers/Login/LoginLayout.jsx
+++ src/containers/Login/LoginLayout.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import styles from './login.scss';
-import { LoginAction } from '../../constants/actionTypes/loginAction';
+import {LoginAction} from '../../constants/actionTypes/loginAction';
 import PropTypes from 'prop-types';
 import Login from './Login';
 import Register from './Register';
@@ -14,40 +14,54 @@ class LoginLayout extends Component {
     pageTab: PropTypes.string,
     changeLoginPage: PropTypes.func,
   }
+
   constructor(props) {
     super(props);
   }
 
   render() {
-    const { pageTab, changeLoginPage } = this.props;
+    const {pageTab, changeLoginPage} = this.props;
     return (
-      <div className={styles.preLogin}>
-        <div className={styles.preLoginImg}>
-          <div className={styles.logo}>
-            <div className={styles.pic}> <img src= {require('../../../assets/img/logo_power.png')} alt=""/></div>
-            <h3>智慧能源运维平台</h3>
-          </div>
-          <img className={styles.bgPic}  src={require('../../../assets/img/bg_01.png')} />
-          <span className={styles.comRegisterInfo}>京ICP备12030847号-2 © 2017-2018 北京动力协合科技有限公司</span>
 
+      <div className={styles.preLogin}>
+        <div className={styles.left}>
+          <div className={styles.logoImg}>
+            <div className={styles.preLoginImg}>
+              <div className={styles.logo}>
+                <div className={styles.pic}><img src={require('../../../assets/img/logo_power.png')} alt=""/></div>
+                <h3>智慧能源运维平台</h3>
+              </div>
+              <img className={styles.bgPic} src={require('../../../assets/img/bg_01.png')}/>
+              <span className={styles.comRegisterInfo}>京ICP备12030847号-2 © 2017-2018 北京动力协合科技有限公司</span>
+
+            </div>
+          </div>
         </div>
-        <div className={styles.preLoginContent}>
-          {pageTab==='login' && <Login changeLoginPage={changeLoginPage} pageTab={pageTab} />}
-          {pageTab==='register' && <Register changeLoginPage={changeLoginPage} pageTab={pageTab} />}
-          {pageTab==='joinIn' && <JoinIn changeLoginPage={changeLoginPage} pageTab={pageTab} />}
-          {pageTab==='forget' && <Forget changeLoginPage={changeLoginPage} pageTab={pageTab} />}
+        <div className={styles.right}>
+          <div className={styles.containerLogin}>
+
+            <div className={styles.preLoginContent}>
+              {pageTab === 'login' && <Login changeLoginPage={changeLoginPage} pageTab={pageTab}/>}
+              {pageTab === 'register' && <Register changeLoginPage={changeLoginPage} pageTab={pageTab}/>}
+              {pageTab === 'joinIn' && <JoinIn changeLoginPage={changeLoginPage} pageTab={pageTab}/>}
+              {pageTab === 'forget' && <Forget changeLoginPage={changeLoginPage} pageTab={pageTab}/>}
+            </div>
+          </div>
         </div>
+
+
       </div>
     );
   }
 }
+
 const mapStateToProps = (state) => ({
   pageTab: state.login.get('pageTab'),
 
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  changeLoginPage: params => dispatch({ type: LoginAction.CHANGE_LOGIN_PAGE_SAGA, params }),
+  changeLoginPage: params => dispatch({type: LoginAction.CHANGE_LOGIN_PAGE_SAGA, params}),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginLayout);
