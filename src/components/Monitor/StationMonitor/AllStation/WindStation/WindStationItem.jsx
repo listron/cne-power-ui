5d18f8223692c4cbc91994031d1ccc613d3148aa
王瑜琪 <greenfish84@126.com>
2018-08-13 16:09:31 +0800

    大小写问题

---
+++ src/components/Monitor/StationMonitor/AllStation/WindStation/WindStationItem.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from './windstation.scss';
+import { Progress, Tabs, Icon, Button, Radio, Switch, Table } from "antd";
+
+// const TabPane = Tabs.TabPane;
+
+
+
+class  WindStationItem extends React.Component{
+    constructor(props,context){
+        super(props,context)
+    }
+    render(){
+        return(
+            <div>
+              <div className={styles.stationCardContainer}>
+              <div className={styles.stationCard}>
+                <div className={styles.stationCardTitle}>圣经山</div>
+                <div className={styles.stationCardProgress}>
+                  <Progress percent={50} showInfo={false} />
+                </div>
+                <div className={styles.stationCardValue}>
+                  <div className={styles.stationMark}>7.42MW</div>
+                  <div>48MW</div>
+                </div>
+                <div className={styles.stationCardWindSpeed}>4.69m/s</div>
+                <div className={styles.stationCardEquipmentNum}>
+                  <div>24台</div>
+                  <div className={styles.stationWarning}>⚠10</div>
+                </div>
+              </div>
+              <div className={styles.stationCard}>
+                <div className={styles.stationCardTitle}>圣经山</div>
+                <div className={styles.stationCardProgress}>
+                  <Progress percent={50} showInfo={false} />
+                </div>
+                <div className={styles.stationCardValue}>
+                  <div className={styles.stationMark}>7.42MW</div>
+                  <div>48MW</div>
+                </div>
+                <div className={styles.stationCardWindSpeed}>4.69m/s</div>
+                <div className={styles.stationCardEquipmentNum}>
+                  <div>24台</div>
+                  <div className={styles.stationWarning}>⚠10</div>
+                </div>
+              </div>
+              <div className={styles.stationCard}>
+                <div className={styles.stationCardTitle}>圣经山</div>
+                <div className={styles.stationCardProgress}>
+                  <Progress percent={50} showInfo={false} />
+                </div>
+                <div className={styles.stationCardValue}>
+                  <div className={styles.stationMark}>7.42MW</div>
+                  <div>48MW</div>
+                </div>
+                <div className={styles.stationCardWindSpeed}>4.69m/s</div>
+                <div className={styles.stationCardEquipmentNum}>
+                  <div>24台</div>
+                  <div className={styles.stationWarning}>⚠10</div>
+                </div>
+              </div>
+              <div className={styles.stationCard}>
+                <div className={styles.stationCardTitle}>圣经山</div>
+                <div className={styles.stationCardProgress}>
+                  <Progress percent={50} showInfo={false} />
+                </div>
+                <div className={styles.stationCardValue}>
+                  <div className={styles.stationMark}>7.42MW</div>
+                  <div>48MW</div>
+                </div>
+                <div className={styles.stationCardWindSpeed}>4.69m/s</div>
+                <div className={styles.stationCardEquipmentNum}>
+                  <div>24台</div>
+                  <div className={styles.stationWarning}>⚠10</div>
+                </div>
+              </div>
+              <div className={styles.stationCard}>
+                <div className={styles.stationCardTitle}>圣经山</div>
+                <div className={styles.stationCardProgress}>
+                  <Progress percent={50} showInfo={false} />
+                </div>
+                <div className={styles.stationCardValue}>
+                  <div className={styles.stationMark}>7.42MW</div>
+                  <div>48MW</div>
+                </div>
+                <div className={styles.stationCardWindSpeed}>4.69m/s</div>
+                <div className={styles.stationCardEquipmentNum}>
+                  <div>24台</div>
+                  <div className={styles.stationWarning}>⚠10</div>
+                </div>
+              </div>
+            </div>
+
+            </div>
+        )
+    }
+}
+export default WindStationItem;
