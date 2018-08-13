6c21c16658c7b414e37e083457a6b0d17bf85679
王瑜琪 <greenfish84@126.com>
2018-08-13 15:40:38 +0800

    修改编译问题

--- src/redux/reducer/monitor/monitorReduce.js
+++ src/redux/reducer/monitor/monitorReduce.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import stationMonitor from './stationMonitor/stationMonitorReducer';
 import deviceMonitorReducer from './stationMonitor/deviceMonitorReducer';
-import singleStation from './stationMonitor/SingleStationReducer';
+import singleStation from './stationMonitor/singleStationReducer';
 
 const monitorReducer = combineReducers({ stationMonitor,deviceMonitorReducer, singleStation });
 
