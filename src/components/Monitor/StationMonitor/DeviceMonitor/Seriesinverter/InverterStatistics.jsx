c03dce6b3e814b51f015113ead9e769c96c5ba4f
zwy <18301417091@163.com>
2018-08-15 18:30:39 +0800

    气象站完善

--- src/components/Monitor/StationMonitor/DeviceMonitor/Seriesinverter/InverterStatistics.jsx
+++ src/components/Monitor/StationMonitor/DeviceMonitor/Seriesinverter/InverterStatistics.jsx
@@ -3,11 +3,10 @@ import PowerProgress from '../DeviceMonitorCommon/PowerProgress';
 import styles from '../eachDeviceMonitor.scss';
 
 function InverterStatistics({ deviceDetail }) {
-  
-  const { devicePower, deviceCapacity, powerDay, powerMonth, powerYear } = deviceDetail;
-  const showDayPower = parseFloat(powerDay) >= 0 ? powerDay: ' -- ';
-  const showMonthPower = parseFloat(powerMonth) >= 0 ? powerDay: ' -- ';
-  const showYearPower = parseFloat(powerYear) >= 0 ? powerDay: ' -- ';
+  let { devicePower, deviceCapacity, powerDay, powerMonth, powerYear } = deviceDetail;
+  powerDay = isNaN(parseFloat(powerDay)) ? ' -- ': parseFloat(powerDay); 
+  powerMonth = isNaN(parseFloat(powerMonth)) ? ' -- ': parseFloat(powerMonth); 
+  powerYear = isNaN(parseFloat(powerYear)) ? ' -- ': parseFloat(powerYear); 
   return (
     <div className={styles.statisticsBox} >
       <div className={styles.deviceIcon}>
@@ -15,16 +14,16 @@ function InverterStatistics({ deviceDetail }) {
       </div>
       <PowerProgress devicePower={devicePower} deviceCapacity={deviceCapacity} />
       <div className={styles.timerDayGen}>
-        <div className={styles.genNum}>{showDayPower}</div>
+        <div className={styles.genNum}>{powerDay}</div>
         <div className={styles.genText}>日发电量 万kWh</div>
       </div>
       <div className={styles.timerGen}>
-        <div className={styles.genNum}>{showMonthPower}</div>
+        <div className={styles.genNum}>{powerMonth}</div>
         <div className={styles.genText}>月发电量 万kWh</div>
       </div>
       <div className={styles.timerGen}>
-        <div className={styles.genNum}>{showYearPower}</div>
-        <div className={styles.genText}>日发电量 万kWh</div>
+        <div className={styles.genNum}>{powerYear}</div>
+        <div className={styles.genText}>年发电量 万kWh</div>
       </div>
     </div>
   )
