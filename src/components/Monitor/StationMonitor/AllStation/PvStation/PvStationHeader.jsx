99a0b0f5c35c265fa075fe3625cf7a937d279708
tiandali <tian.chengli@outlook.com>
2018-08-17 11:35:14 +0800

    进度条

--- src/components/Monitor/StationMonitor/AllStation/PvStation/PvStationHeader.jsx
+++ src/components/Monitor/StationMonitor/AllStation/PvStation/PvStationHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from './pvStation.scss';
-import { Progress } from "antd";
 import CommonProgress from '../../../../Common/CommonProgress'
 class pvStationHeader extends React.Component {
   static propTypes = {
--- src/components/Monitor/StationMonitor/AllStation/WindStation/WindStationHeader.jsx
+++ src/components/Monitor/StationMonitor/AllStation/WindStation/WindStationHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from './windStation.scss';
-import { Progress } from "antd";
 import CommonProgress from '../../../../Common/CommonProgress'
 
 class windStationHeader extends React.Component {
