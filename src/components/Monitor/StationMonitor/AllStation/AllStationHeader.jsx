14a1e0c0e22204df9fb9c9277eb141d378617cf7
tiandali <tian.chengli@outlook.com>
2018-08-17 11:35:37 +0800

    进度条

--- src/components/Monitor/StationMonitor/AllStation/AllStationHeader.jsx
+++ src/components/Monitor/StationMonitor/AllStation/AllStationHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from './allStation.scss';
-import { Progress ,Icon } from 'antd';
+import { Icon } from 'antd';
 import CommonProgress from '../../../Common/CommonProgress'
 
 
