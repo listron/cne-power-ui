7bae27c50602d698b84e0ae1a7eee6b4e086ceb1
王瑜琪 <greenfish84@126.com>
2018-08-27 10:58:24 +0800

    添加获取单电站和多电站告警统计的数据处理

--- src/constants/actionTypes/monitor/alarmAction.js
+++ src/constants/actionTypes/monitor/alarmAction.js
@@ -5,6 +5,8 @@ module.exports = {
     ALARM_FETCH: null,//loading
     GET_REALTIME_ALARM_SAGA: null,//	获取多电站实时告警列表
     GET_HISTORY_ALARM_SAGA: null,//获取历史告警列表
+    GET_STATIONS_ALARM_STATISTIC_SAGA: null,//获取多电站告警统计
+    GET_SINGLESTATION_ALARM_STATISTIC_SAGA: null,//获取单电站告警统计
     CHANGE_ALARM_STORE_SAGA: null,
     CHANGE_ALARM_STORE: null,
     GET_ALARM_FETCH_SUCCESS: null,
