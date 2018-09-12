0c6725d947bc1d35cc5424a3e6ee3f1af644b513
zwy <18301417091@163.com>
2018-09-12 21:32:48 +0800

    调整测点页面数据

--- src/components/System/Station/AlarmManage/AlarmManageList.jsx
+++ src/components/System/Station/AlarmManage/AlarmManageList.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import styles from './alarmManage.scss';
-import { Table } from 'antd';
+import { Table, Upload, Button } from 'antd';
 import PropTypes from 'prop-types';
+import Cookie from 'js-cookie';
 
 class AlarmManageList extends Component {
   static propTypes = {
@@ -24,6 +25,11 @@ class AlarmManageList extends Component {
       sortField: sorter.field,
       sortOrder: sorter.order==='ascend'?'asc':'desc',
     })
+  }
+
+  handleUpload = ({file,fileList}) => {
+    console.log(file)
+    console.log(fileList)
   }
 
   render() {
@@ -53,8 +59,32 @@ class AlarmManageList extends Component {
       }
     ];
     const { loading, alarmList } = this.props;
+
+// -- totest 代码=> 准备删除上面的cookie upload button引入
+    const authData = Cookie.get('authData') || null;
+    const imageProps = {
+			action: 'http://10.10.15.51/api/v3/uploadfile',
+      onChange: this.handleUpload,
+			listType: "picture-card",
+      headers:{'Authorization': 'bearer ' + JSON.parse(authData)},
+      data: (file) => {
+        console.log(file)
+        return {
+          name:' zhang ',
+          test: 'testing',
+          file,
+        }
+      }
+    };
+    
+
     return (
       <div className={styles.deviceManageList}>
+        <Upload
+          { ...imageProps }
+        >
+          <Button>上传试试</Button>
+        </Upload>
         <Table
           loading={loading}
           onChange={this.tableChange}
