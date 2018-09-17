b0899ebbd6922f4fc43ec51bf228f4707d608353
zwy <18301417091@163.com>
2018-09-17 16:53:51 +0800

    调整theme引入

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,24 @@
             }
           }
         ]
+      },{
+        test: /\.less$/,
+        exclude: /src/,
+        use: [{
+          loader: "style-loader" 
+        }, {
+            loader: "css-loader",
+        }, {
+          loader: "less-loader",
+          options: {
+            importLoaders: 1,
+            modifyVars: {
+              'primary-color': '#199475',
+              'link-color': '#199475',
+            },
+            javascriptEnabled: true,
+          },
+        }]
       }, {
         test: /\.scss$/,
         use: [{
