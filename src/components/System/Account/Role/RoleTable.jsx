0f8a6ed9cd79dcdbc3875a1fa4708897c15d8a4e
zwy <18301417091@163.com>
2018-09-27 18:47:17 +0800

    基本页面结构完成

--- src/components/System/Account/Role/RoleTable.jsx
+++ src/components/System/Account/Role/RoleTable.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Select, Popover } from 'antd';
+import { Table, Select, Popover, Button, Icon } from 'antd';
 import WarningTip from '../../../Common/WarningTip';
 import PropTypes from 'prop-types';
 import styles from './role.scss';
@@ -22,7 +22,6 @@ class RoleTable extends Component {
     this.state = {
       showWarningTip: false,
       warningTipText: '',
-      hiddenWarningTipCancelText: false
     };
   }
 
@@ -32,10 +31,29 @@ class RoleTable extends Component {
     })
   }
 
-  cancelRowSelect = () => {
-    this.props.changeRoleStore({
-      selectedRole:[]
+  onRoleAdd = () =>{//进入添加角色页
+    this.props.changeRoleStore({ showPage: 'create' });
+  }
+
+  onConfirmWarningTip = () => {
+    this.setState({
+      showWarningTip: false,
+    });  
+  }
+
+  getRightArr(rightData, frontText='') { // 递归生成权限数组['一级-二级-三级...',...]
+    let dataInfoArr = [];
+    rightData && rightData.length > 0 && rightData.forEach(e=>{
+      const hasChildRight = e && e.childRightData && e.childRightData.length > 0
+      if(hasChildRight){
+        const innerData = this.getRightArr(e.childRightData, e.rightName);
+        dataInfoArr.push(...innerData);
+      }else{
+        const rightText = `${frontText?`${frontText}-`:''}${e.rightName}`
+        e && e.rightName && dataInfoArr.push(rightText);
+      }
     })
+    return dataInfoArr;
   }
   
   createRoloeColumn = () => [
@@ -53,120 +71,71 @@ class RoleTable extends Component {
       title: '功能定义',
       dataIndex: 'rightData',
       key: 'rightData',
-      render: (text,record)=>{
-        const rightArr = this.renderAuth(text);
+      render: (rightData,record)=>{
+        const rightArr = this.getRightArr(rightData,'');
         const {roleDesc}=record;
         const content = (
           <div className={styles.tooltip}>{rightArr.map((item,index)=>(<span key={index}>{item}</span>))}</div>
         );
         return (
           <Popover  content={content}  placement="right" trigger="hover"  title={roleDesc}>
-            <div className={styles.menu}>{rightArr.join('|')}</div>
+            <div className={styles.menu}>{rightArr.join(' | ')}</div>
           </Popover>
         );
       }
     }
   ];
 
-  renderAuth(rightData) {
-    let result = [];
-    let rightEl = ''
-    this.renderAuthEl(rightData, result, rightEl);
-    return result;
-  }
-
-  renderAuthEl(arr, result, rightEl) {
-    for(var i = 0; i < arr.length; i++) {
-      let name;
-      if(rightEl === '') {
-        name = arr[i].rightName;
-      } else {
-        name = rightEl+'-'+arr[i].rightName;
-      }
-      if(arr[i].childRightData instanceof Array && arr[i].childRightData.length > 0) {
-        this.renderAuthEl(arr[i].childRightData, result, name);
-      } else {
-        result.push(name);
-      }
-    }
-  }
-
-/*
-  onRoleAdd = () =>{//进入添加角色页
-    this.props.changeRoleStore({showPage: 'create'});
-  }
-  
-  onConfirmWarningTip = () => {
-    this.setState({
-      showWarningTip: false,
-    });  
+  cancelRowSelect = () => {
+    this.props.changeRoleStore({
+      selectedRole:[]
+    })
   }
 
   roleHandle = (value) => {//编辑
-    const { selectedRole } = this.props;
-    if(value === 'edit'){
-      if(selectedRole.some(item=>item.isPre===0)) {
-        this.setState({
-          showWarningTip: true,
-          warningTipText: '不得编辑预设角色!',
-          hiddenWarningTipCancelText: true
-        });
-      } else {
-        this.props.changeRoleStore({
-          showPage: 'edit'
-        });
-      }
-    }else if(value === 'delete'){
-      if(selectedRole.some(item=>item.isPre===0)) {
-        this.setState({
-          showWarningTip: true,
-          warningTipText: '不得删除预设角色!',
-          hiddenWarningTipCancelText: true
-        });
-      } else {
-        this.props.onDeleteRole({
-          roleId: selectedRole.map(e=>e.roleId).join(',')
-        });
-      }
+    const { selectedRole, changeRoleStore, onDeleteRole } = this.props;
+    const isPreUser = selectedRole.some(item=>!item.isPre);
+    if(isPreUser){
+      const warningTipText = `不得${value === 'edit'?'编辑':'删除'}预设角色`;
+      this.setState({
+        showWarningTip: true,
+        warningTipText,
+      })
+    }else{
+      value === 'edit'?changeRoleStore({
+        showPage: 'edit',
+      }):onDeleteRole({
+        roleId: selectedRole.map(e=>e.roleId).join(',')
+      });
     }
   }
 
-  createHandleOption = (rightHandler) => {//生成操作下拉框
-    const { selectedRole } = this.props;
-    // const roleDeleteRight = rightHandler && rightHandler.split(',').includes('account_role_delete');
-    // const roleUpdateRight = rightHandler && rightHandler.split(',').includes('account_role_update');
-    // const showAllHandler = roleDeleteRight || roleUpdateRight;   权限管理 -- todo
-    return (
-      <Select onChange={this.roleHandle} value="操作" placeholder="操作" dropdownMatchSelectWidth={false} dropdownClassName={styles.handleDropdown}>
-        <Option value="edit" disabled={selectedRole.length>1||selectedRole.length===0}>编辑</Option>
-        <Option value="delete" disabled={selectedRole.length===0}>删除</Option>
-      </Select>
-    );
-  }
-
-  
-*/
   render(){
     const { selectedRole, roleData, loading, showPage } = this.props;
-    // const { showWarningTip, warningTipText, hiddenWarningTipCancelText } = this.state;
-    // const rightHandler = localStorage.getItem('rightHandler');
-    // const roleCreateRight = rightHandler && rightHandler.split(',').includes('account_role_create');
-    // const userImportRight = rightHandler && rightHandler.split(',').includes('account_user_batchImport');
+    const { showWarningTip, warningTipText } = this.state;
+    const rightHandler = localStorage.getItem('rightHandler');
+    const roleCreateRight = rightHandler && rightHandler.split(',').includes('account_role_create');
+    const roleDeleteRight = rightHandler && rightHandler.split(',').includes('account_role_delete');
+    const roleUpdateRight = rightHandler && rightHandler.split(',').includes('account_role_update');
+    const roleConfigRight = rightHandler && rightHandler.split(',').includes('account_role_config');
     return (
       <div className={styles.roleList} style={{display: showPage==='list'?'flex':'none'}}>
-      {/* {showWarningTip && <WarningTip onOK={this.onConfirmWarningTip} value={warningTipText} hiddenCancel={hiddenWarningTipCancelText} />} */}
-        {/* <div className={styles.roleListTop} >
-          <div>
-            <Button className={styles.addRole} onClick={this.onRoleAdd}>
-              <Icon type="plus" />
-              <span className={styles.text}>角色</span>
-            </Button>
-            <div className={styles.handleRole}>
-              {this.createHandleOption(rightHandler)}
+      {showWarningTip && <WarningTip onOK={this.onConfirmWarningTip} value={warningTipText} />}
+        <div className={styles.roleContent}>
+          <div className={styles.roleListTop} >
+            <div>
+              {roleCreateRight && <Button className={styles.addRole} onClick={this.onRoleAdd}>
+                <Icon type="plus" />
+                <span className={styles.text}>角色</span>
+              </Button>}
+              <div className={styles.handleRole}>
+              {roleConfigRight && <Select onChange={this.roleHandle} value="操作" placeholder="操作" dropdownMatchSelectWidth={false} dropdownClassName=  {styles.handleDropdown}>
+                {roleUpdateRight && <Option value="edit" disabled={selectedRole.length !== 1}>编辑</Option>}
+                {roleDeleteRight && <Option value="delete" disabled={selectedRole.length===0}>删除</Option>}
+              </Select>}
+              </div>
             </div>
           </div>
-        </div> */}
-        <div className={styles.roleContent}>
           <Table 
             loading={loading}
             rowKey={(record)=>{return record.roleId}} 
