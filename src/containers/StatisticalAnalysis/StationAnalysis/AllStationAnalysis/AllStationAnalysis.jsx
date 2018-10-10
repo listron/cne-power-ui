102c95178219b1af1308d18ae67b3ed8d889f424
zwy <18301417091@163.com>
2018-10-10 16:19:35 +0800

    调整文件

--- src/containers/StatisticalAnalysis/StationAnalysis/AllStationAnalysis/AllStationAnalysis.jsx
+++ src/containers/StatisticalAnalysis/StationAnalysis/AllStationAnalysis/AllStationAnalysis.jsx
@@ -3,9 +3,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import styles from "./allStationAnalysis.scss";
 import PropTypes from "prop-types";
-import AllStationStatistic from '../../../../components/statisticalAnalysis/stationAnalysis/allStationAnalysis/AllStationStatistic';
-import SingStationStatistic from '../../../../components/statisticalAnalysis/stationAnalysis/allStationAnalysis/SingStationStatistic.jsx';
-import { allStationAnalysisAction } from './AllStationAnalysisAction';
+// import AllStationStatistic from '../../../../components/StatisticalAnalysis/StationAnalysis/AllStationAnalysis/AllStationStatistic';
+// import SingStationStatistic from '../../../../components/StatisticalAnalysis/StationAnalysis/AllStationAnalysis/SingStationStatistic';
 import CommonBreadcrumb from '../../../../components/Common/CommonBreadcrumb';
 import Footer from '../../../../components/Common/Footer';
 
@@ -46,8 +45,8 @@ class ALarmStatistic extends Component {
       <div className={styles.allStationAnalysisBox} >
         <CommonBreadcrumb  {...breadCrumbData} style={{ marginLeft: '38px' }} />
         <div className={styles.allStationStatistic}>     
-          <AllStationStatistic {...this.props} />
-          {showPage === 'single' && <SingStationStatistic {...this.props} />}     
+          {/* <AllStationStatistic {...this.props} /> */}
+          {/* {showPage === 'single' && <SingStationStatistic {...this.props} />}      */}
          
         </div>
         <Footer />
