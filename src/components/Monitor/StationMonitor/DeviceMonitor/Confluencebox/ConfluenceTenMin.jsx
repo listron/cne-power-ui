fc6a37e77a1f49bd7b2219594fdbd8078de7d7ac
cne_yusq <yusq@cnegroup.com>
2018-10-22 18:49:01 +0800

    删除冗余

--- src/components/Monitor/StationMonitor/DeviceMonitor/Confluencebox/ConfluenceTenMin.jsx
+++ src/components/Monitor/StationMonitor/DeviceMonitor/Confluencebox/ConfluenceTenMin.jsx
@@ -21,95 +21,111 @@ function ConfluenceTenMin({ deviceTenMin, loading }) {
       xTime.push(moment(e.utc).format('YYYY-MM-DD HH:mm:ss'));
       dispersionRatio.push(e.dispersionRatio);
     });
-    console.log(deviceTenMin);
-    // if(deviceTenMin && deviceTenMin.length>0){
-      for(let i=0;i<16;i++){
-        HL.push();
-        HL[i] = deviceTenMin.length > 0 && deviceTenMin.map((e,index)=>{
-          console.log(e);
-          return e.hLArr[i];
-          
-        });
+    for(let i=0;i<16;i++){
+      HL.push();
+      HL[i] = deviceTenMin.length > 0 && deviceTenMin.map((e,index)=>{
+        console.log(e);
+        return e.hLArr[i];
+        
+      });
+    }
+    const HLNamesArr = HLNames.map((e,i)=>{
+      return {
+        name: e,
+        nameTextStyle: {
+          color: lineColor,
+        },
+        type: 'line',
+        lineStyle: {
+          type: 'solid',
+          color: HLColors[i],
+          width: 1,
+        },
+        label: {
+          normal: {
+            show: false
+          }
+        },
+        itemStyle: {
+          opacity: 0,
+        },
+        yAxisIndex: 0,
+        data: HL[i],
       }
-      const HLNamesArr = HLNames.map((e,i)=>{
-        return {
-          name: e,
+    });
+  
+  
+    const filterDispersionRatio = deviceTenMin.filter(e=>e.dispersionRatio);
+    const confluenceTenMinGraphic = filterDispersionRatio.length===0  ? showNoData : hiddenNoData;
+    const option = {
+      graphic: confluenceTenMinGraphic,
+      title: {
+        text: '时序图',
+        textStyle: {
+          color: lineColor,
+          fontSize: 14,
+        },
+        left: 60
+      },
+      legend: {
+        data:['离散率',...HLNames],
+        top: 24,
+        itemWidth: 20,
+        itemHeight: 4, 
+      },
+      tooltip: {
+        show: true,
+        trigger: 'axis',
+        axisPointer: {
+          crossStyle:{
+            color: '#dfdfdf',
+            width: 1,
+            type: 'dotted',
+          }
+        },
+        position: function (point, params, dom, rect, size) {
+          // 固定在顶部
+          return [point[0], '10%'];
+        },
+        formatter: (param) => {
+          const HLToolTips = HLNames.map((e,i)=>{
+            return `<div style="padding-left: 5px;background:#fff; line-height: 20px;height:20px;" ><span style="display: inline-block; background:${HLColors[i]}; width:6px; height:6px; border-radius:100%;"></span> ${e}: ${param && param[i+1] && param[i+1].value || '--'}</div>`;
+          });
+          return `<div style="width: 128px; height: 380px;color: #666; line-height: 14px;font-size:12px;background: #fff;box-shadow:0 1px 4px 0 rgba(0,0,0,0.20);border-radius:2px;">
+            <div style="border-bottom: 1px solid #dfdfdf;padding-left: 5px;line-height: 25px;height:25px;" >${param && param[0] && param[0].name || '--'}</div>
+            <div style="padding-left: 5px;line-height: 20px;height:20px;" ><span style="display: inline-block; background:#ffffff; border:1px solid #3e97d1; width:6px; height:6px; border-radius:100%;"></span> 离散率: ${param && param[0] && param[0].value || '--'}</div>
+            ${HLToolTips.join('')}
+          </div>`;
+        },
+        extraCssText:'background: rgba(0,0,0,0);',
+      },
+      grid: {
+        top: 95,
+        containLabel: true,
+      },
+      xAxis: {
+        type: 'category',
+        data: xTime,
+        axisTick: {
+          show: false,
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#dfdfdf',
+          },
+        },
+        axisLabel: {
+          color: lineColor,
+        },
+      },
+      yAxis: [
+        {
+          name: '电流(A)',
           nameTextStyle: {
             color: lineColor,
           },
-          type: 'line',
-          lineStyle: {
-            type: 'solid',
-            color: HLColors[i],
-            width: 1,
-          },
-          label: {
-            normal: {
-              show: false
-            }
-          },
-          itemStyle: {
-            opacity: 0,
-          },
-          yAxisIndex: 0,
-          data: HL[i],
-        }
-      });
-    
-    
-      const filterDispersionRatio = deviceTenMin.filter(e=>e.dispersionRatio);
-      const confluenceTenMinGraphic = filterDispersionRatio.length===0  ? showNoData : hiddenNoData;
-      const option = {
-        graphic: confluenceTenMinGraphic,
-        title: {
-          text: '时序图',
-          textStyle: {
-            color: lineColor,
-            fontSize: 14,
-          },
-          left: 60
-        },
-        legend: {
-          data:['离散率',...HLNames],
-          top: 24,
-          itemWidth: 20,
-          itemHeight: 4, 
-        },
-        tooltip: {
-          show: true,
-          trigger: 'axis',
-          axisPointer: {
-            crossStyle:{
-              color: '#dfdfdf',
-              width: 1,
-              type: 'dotted',
-            }
-          },
-          position: function (point, params, dom, rect, size) {
-            // 固定在顶部
-            return [point[0], '10%'];
-          },
-          formatter: (param) => {
-            const HLToolTips = HLNames.map((e,i)=>{
-              return `<div style="padding-left: 5px;background:#fff; line-height: 20px;height:20px;" ><span style="display: inline-block; background:${HLColors[i]}; width:6px; height:6px; border-radius:100%;"></span> ${e}: ${param && param[i+1] && param[i+1].value || '--'}</div>`;
-            });
-            return `<div style="width: 128px; height: 380px;color: #666; line-height: 14px;font-size:12px;background: #fff;box-shadow:0 1px 4px 0 rgba(0,0,0,0.20);border-radius:2px;">
-              <div style="border-bottom: 1px solid #dfdfdf;padding-left: 5px;line-height: 25px;height:25px;" >${param && param[0] && param[0].name || '--'}</div>
-              <div style="padding-left: 5px;line-height: 20px;height:20px;" ><span style="display: inline-block; background:#ffffff; border:1px solid #3e97d1; width:6px; height:6px; border-radius:100%;"></span> 离散率: ${param && param[0] && param[0].value || '--'}</div>
-              ${HLToolTips.join('')}
-            </div>`;
-          },
-          extraCssText:'background: rgba(0,0,0,0);',
-        },
-        grid: {
-          top: 95,
-          containLabel: true,
-        },
-        xAxis: {
-          type: 'category',
-          data: xTime,
-          axisTick: {
-            show: false,
+          splitLine:{
+            show:false
           },
           axisLine: {
             lineStyle: {
@@ -119,83 +135,65 @@ function ConfluenceTenMin({ deviceTenMin, loading }) {
           axisLabel: {
             color: lineColor,
           },
+          axisTick: {
+            show: false,
+          },
         },
-        yAxis: [
-          {
-            name: '电流(A)',
-            nameTextStyle: {
-              color: lineColor,
-            },
-            splitLine:{
-              show:false
-            },
-            axisLine: {
-              lineStyle: {
-                color: '#dfdfdf',
-              },
-            },
-            axisLabel: {
-              color: lineColor,
-            },
-            axisTick: {
-              show: false,
-            },
+        {
+          name: '离散率',
+          nameTextStyle: {
+            color: lineColor,
           },
-          {
-            name: '离散率',
-            nameTextStyle: {
-              color: lineColor,
-            },
-            splitLine:{
-              show:false
-            },
-            axisLine: {
-              lineStyle: {
-                color: '#dfdfdf',
-              },
-            },
-            axisLabel: {
-              color: lineColor,
-            },
-            axisTick: {
-              show: false,
-            },
-          }
-        ],
-        series: [
-          {
-            name: '离散率',
-            nameTextStyle: {
-              color: lineColor,
-            },
-            type: 'line',
+          splitLine:{
+            show:false
+          },
+          axisLine: {
             lineStyle: {
-              type: 'dotted',
-              color: '#3e97d1',
-              width: 1,
+              color: '#dfdfdf',
             },
-            // areaStyle: {
-            //   normal: {
-            //     opacity: 0.2,
-            //   }
-            // },
-            label: {
-              normal: {
-                show: false
-              }
-            },
-            itemStyle: {
-              opacity: 0,
-            },
-            yAxisIndex: 1,
-            data: dispersionRatio,
           },
-          ...HLNamesArr,
-        ]
-      };
-      confluenceChart.setOption(option);
-      confluenceChart.resize();
-    // }
+          axisLabel: {
+            color: lineColor,
+          },
+          axisTick: {
+            show: false,
+          },
+        }
+      ],
+      series: [
+        {
+          name: '离散率',
+          nameTextStyle: {
+            color: lineColor,
+          },
+          type: 'line',
+          lineStyle: {
+            type: 'dotted',
+            color: '#3e97d1',
+            width: 1,
+          },
+          // areaStyle: {
+          //   normal: {
+          //     opacity: 0.2,
+          //   }
+          // },
+          label: {
+            normal: {
+              show: false
+            }
+          },
+          itemStyle: {
+            opacity: 0,
+          },
+          yAxisIndex: 1,
+          data: dispersionRatio,
+        },
+        ...HLNamesArr,
+      ]
+    };
+    confluenceChart.setOption(option);
+    confluenceChart.resize();
+    
   }
   return (
     <div id="confluence_monitor_tenMin" style={{height:"335px",marginTop: '20px'}}></div>
