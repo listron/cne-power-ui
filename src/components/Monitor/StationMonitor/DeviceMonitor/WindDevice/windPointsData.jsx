ba9ea5608d7ac0f6ef27416728bcffe24c60c4ff
haojianling <591191280@qq.com>
2018-11-23 13:56:51 +0800

    风电站测点数据排序

--- src/components/Monitor/StationMonitor/DeviceMonitor/WindDevice/windPointsData.jsx
+++ src/components/Monitor/StationMonitor/DeviceMonitor/WindDevice/windPointsData.jsx
@@ -3,8 +3,9 @@ import styles from '../eachDeviceMonitor.scss'
 
 function DevicePointsData({ devicePointData, deviceDetail }) {
   devicePointData = devicePointData.sort((a, b) => (a.devicePointCode.localeCompare(b.devicePointCode)))
-  let devicePointIECGroup = [...new Set(devicePointData.map(e => e.devicePointIECGroup))]
-  // console.log('devicePointIECGroup',devicePointIECGroup)
+  // let devicePointIECGroup1 = [...new Set(devicePointData.map(e => e.devicePointIECGroup))]
+  let devicePointIECGroup = ['整机系统', '变桨系统', '传动系统', '发电机', '变频器', '偏航系统', '机舱系统', '塔筒系统', '箱变系统', '测风塔', '其他']
+  // console.log('devicePointIECGroup',devicePointIECGroup,devicePointIECGroup1)
   let ponitGroup = devicePointIECGroup.map(e => {
     return devicePointData.filter((item, index) => {
       if (item.devicePointIECGroup === e) {
@@ -24,50 +25,28 @@ function DevicePointsData({ devicePointData, deviceDetail }) {
   })
 
   // console.log('pointListGroups', pointListGroups)
-
-
-  // let pointListGroup = [], startIndex = 0;
-  // do {
-  //   let eachPointGroup = devicePointData.slice(startIndex, startIndex + 10);
-  //   startIndex += 10;
-  //   pointListGroup.push(eachPointGroup);
-  // } while (startIndex < devicePointData.length);
-
   return (
     <div className={styles.pointData} >
       <div className={styles.pointTitle}>{deviceDetail.deviceTypeName || ''}实时测点数据</div>
       <div className={styles.pointDataList} >
-        {
-          
-          pointListGroups.length > 0 && pointListGroups.map((item, key) => {
-            return  item.map((e, i) => {
-            return (<div className={styles.eachGroup} key={i}>
-              {i===0 && <div className={styles.groupName}>{devicePointIECGroup[key]}</div>}
-              {e.map(eachPoints => {
-                let pointValue = eachPoints.devicePointValue;
-                pointValue = (pointValue || parseFloat(pointValue) === 0) ? pointValue : '--';
-                return (
-                  <div className={styles.eachData} key={eachPoints.devicePointCode}>
-                    <p className={styles.pointName}>{eachPoints.devicePointName}</p>
-                    <p className={styles.pointValue}>{pointValue}{eachPoints.devicePointUnit || ''}</p>
-                  </div>)
-              })}
-            </div>)
+        {pointListGroups.length > 0 && pointListGroups.map((item, key) => {
+          return item.map((e, i) => {
+            if (e.length > 0) {
+              return (<div className={styles.eachGroup} key={i}>
+                {i === 0 && <div className={styles.groupName}>{devicePointIECGroup[key]}</div>}
+                {e.map(eachPoints => {
+                  let pointValue = eachPoints.devicePointValue;
+                  pointValue = (pointValue || parseFloat(pointValue) === 0) ? pointValue : '--';
+                  return (
+                    <div className={styles.eachData} key={eachPoints.devicePointCode}>
+                      <p className={styles.pointName}>{eachPoints.devicePointName}</p>
+                      <p className={styles.pointValue}>{pointValue}{eachPoints.devicePointUnit || ''}</p>
+                    </div>)
+                })}
+              </div>)
+            }
           })
-          
-        })
-        
-        }
-        {/* {pointListGroup.map((e, i) => (<div className={styles.eachGroup} key={i}>
-          {e.map(eachPoints => {
-            let pointValue = eachPoints.devicePointValue;
-            pointValue = (pointValue || parseFloat(pointValue) === 0) ? pointValue : '--';
-            return (<div className={styles.eachData} key={eachPoints.devicePointCode}>
-              <p className={styles.pointName}>{eachPoints.devicePointName}</p>
-              <p className={styles.pointValue}>{pointValue}{eachPoints.devicePointUnit || ''}</p>
-            </div>)
-          })}
-        </div>))} */}
+        }) }
       </div>
     </div>
 
