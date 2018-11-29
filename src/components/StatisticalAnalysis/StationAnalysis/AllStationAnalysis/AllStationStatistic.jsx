1769826cb6f06f34609ff059748ecd7f118930dc
tiandali <tian.chengli@outlook.com>
2018-11-29 17:18:46 +0800

    电站分析有无数据接口加stationType

--- src/components/StatisticalAnalysis/StationAnalysis/AllStationAnalysis/AllStationStatistic.jsx
+++ src/components/StatisticalAnalysis/StationAnalysis/AllStationAnalysis/AllStationStatistic.jsx
@@ -48,11 +48,11 @@ class AllStationStatistic extends React.Component {
     const stationTypeOne = this.stationIsOneType();
     let stationTypes;
     if (stationTypeOne) {
-     stationTypes =stations.getIn([0, 'stationType']);
-     changeAllStationStore({stationType:stationTypes})
+      stationTypes = stations.getIn([0, 'stationType']);
+      changeAllStationStore({ stationType: stationTypes })
     }
     // console.log(typeof(stationTypes));
-    changeAllStationStore({ year: [`${currentYear}`], month: currentMonth, powerSelectMonth: currentMonth,})
+    changeAllStationStore({ year: [`${currentYear}`], month: currentMonth, powerSelectMonth: currentMonth, })
     getAllStationAvalibaData(
       {
         userId: userId,
@@ -393,7 +393,7 @@ class AllStationStatistic extends React.Component {
         <i className="iconfont icon-filter"></i>
       </div>
     );
-     const stationTypeOne = this.stationIsOneType();
+    const stationTypeOne = this.stationIsOneType();
 
     const { stationType, stations, dateType, year, allStationAvalibaData, allStationStatisticData, getAllStationStatisticData, selectYear, changeAllStationStore } = this.props;
     const { showStationSelect } = this.state;
@@ -403,13 +403,13 @@ class AllStationStatistic extends React.Component {
           <div className={styles.AlarmStatisticByTypeBox} >
             <div className={styles.singleAlarmFilter} >{operations}</div>
             <div className={styles.componentContainer}>
-            <TimeSelect showDayPick={false} onChange={this.onTimeChange} />
-            <PlanCompletionRate dateType={dateType} stationType={stationType} allStationAvalibaData={allStationAvalibaData} allStationStatisticData={allStationStatisticData} getAllStationStatisticData={getAllStationStatisticData} year={year} selectYear={selectYear}
-              changeAllStationStore={changeAllStationStore} />
-            <TargetTabs {...this.props} />
-            <StationStatisticList {...this.props} />
-          </div>
-        
+              <TimeSelect showDayPick={false} onChange={this.onTimeChange} />
+              <PlanCompletionRate dateType={dateType} stationType={stationType} allStationAvalibaData={allStationAvalibaData} allStationStatisticData={allStationStatisticData} getAllStationStatisticData={getAllStationStatisticData} year={year} selectYear={selectYear}
+                changeAllStationStore={changeAllStationStore} />
+              <TargetTabs {...this.props} />
+              <StationStatisticList {...this.props} />
+            </div>
+
           </div>
           :
           <Tabs type="card" tabBarExtraContent={operations} activeKey={stationType} onChange={this.queryTargetData} >
