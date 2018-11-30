ef05a13948040774843e4bac6f1d4fa624c7b0b8
haojianling <591191280@qq.com>
2018-11-30 11:21:27 +0800

    从电站上传 upload  2 电站监控 出力图跳到资源分析 3 后台增加一个电站分析的字段，前端修改（电站分析） 4 增加导入功率曲线的修改  5 功率曲线模块的条件查询

--- src/components/Monitor/StationMonitor/SingleStation/PvStation/PvStation.jsx
+++ src/components/Monitor/StationMonitor/SingleStation/PvStation/PvStation.jsx
@@ -150,8 +150,8 @@ class PvStation extends Component {
       <div className={styles.pvStation}  >
         <PvStationTop {...this.props} stationCode={stationCode} hiddenStationList={this.state.hiddenStationList} />
         <div className={styles.outputPowerDiagram}>
-          <OutputTenMin {...this.props} yXaisName={'辐射(W/m²)'} />
-          <PowerDiagramTenMin {...this.props} />
+          <OutputTenMin {...this.props} yXaisName={'辐射(W/m²)'} stationCode={stationCode} />
+          <PowerDiagramTenMin {...this.props}stationCode={stationCode}  />
         </div>
         <CardSection {...this.props} stationCode={stationCode} />
         {/* 设备类型流程图切换 */}
