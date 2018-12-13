928cd00c355b9e9931e6f09105ff4c1624856130
zwy <18301417091@163.com>
2018-12-13 17:41:41 +0800

    边界判定bug添加

--- src/components/Monitor/StationMonitor/SingleStation/WindStation/WindStationTop.jsx
+++ src/components/Monitor/StationMonitor/SingleStation/WindStation/WindStationTop.jsx
@@ -13,7 +13,7 @@ const ValueFormat = ({ value,  points }) => { // value必为数值 或 '--'。
   if (value === '--' || (!points && points === 0)) { // value无值，或不需对数据进行浮点处理
     return <span>{value}</span>;
   } else { // value为数值且有浮点数展示要求
-    let stringValue = value.toFixed(points);
+    let stringValue = value.toFixed(points) || '';
     const valueArr = stringValue.split('.');
     return (
       <span className={styles.valueFormat}>
