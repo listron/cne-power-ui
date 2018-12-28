8a4ac61f5230b9dcbc6fdd975a9fd036dd9e50b0
tiandali <tian.chengli@outlook.com>
2018-12-28 14:38:25 +0800

    更改(MJ/m²)

--- src/components/Monitor/StationMonitor/SingleStation/SingleStationCommon/PowerDiagramTenMin.jsx
+++ src/components/Monitor/StationMonitor/SingleStation/SingleStationCommon/PowerDiagramTenMin.jsx
@@ -143,7 +143,7 @@ class PowerDiagramTenMin extends Component {
             }
           }
         }, {
-          name: chartType === 'wind' ? '平均风速(m/s)' : `${intervalTime === 0 ? '累计辐射(MJ/m²)' : (intervalTime === 1 ? '累计辐射(MJ/m)' : '累计辐射(MJ/m)')}`,
+          name: chartType === 'wind' ? '平均风速(m/s)' : `${intervalTime === 0 ? '累计辐射(MJ/m²)' : (intervalTime === 1 ? '累计辐射(MJ/m²)' : '累计辐射(MJ/m²)')}`,
           type: 'value',
           axisLabel: {
             formatter: '{value}',
