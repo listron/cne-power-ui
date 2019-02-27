ad2aea36e66e0e1aaa643ee0af18b385d3df00c6
haojianling <591191280@qq.com>
2019-02-27 09:53:07 +0800

    暂时修改

--- src/components/StatisticalAnalysis/StationAnalysis/ScoreAnalysis/PvStationTable.jsx
+++ src/components/StatisticalAnalysis/StationAnalysis/ScoreAnalysis/PvStationTable.jsx
@@ -6,13 +6,145 @@ import styles from "./scoreAnalysis.scss";
 
 export default function table(props) {
     const { sigleData } = props;
-    const indexList = sigleData.indexList || [];
+    // const indexList = sigleData.indexList || [];
+    const indexList=[
+        {
+            "finishStatus":null,
+            "indexCode":"1",
+            "indexContent":"实际上网电量/计划发电量*100%",
+            "indexIncrDecrStandard":"1",
+            "indexIncrDecrValue":"3",
+            "indexLowerLimit":"95",
+            "indexName":"计划完成率",
+            "indexPercent":"40",
+            "indexTypeCode":"1",
+            "indexTypeName":"发电水平",
+            "indexUnit":"%",
+            "indexUpperLimit":"105",
+            "scoreLevel":null,
+            "scoreValue":null
+        },
+        {
+            "finishStatus":null,
+            "indexCode":"2",
+            "indexContent":"实际PR/计划PR*100%",
+            "indexIncrDecrStandard":"1",
+            "indexIncrDecrValue":"2",
+            "indexLowerLimit":"95",
+            "indexName":"PR完成率",
+            "indexPercent":"20",
+            "indexTypeCode":"1",
+            "indexTypeName":"发电水平",
+            "indexUnit":"%",
+            "indexUpperLimit":"105",
+            "scoreLevel":null,
+            "scoreValue":null
+        },
+        {
+            "finishStatus":null,
+            "indexCode":"3",
+            "indexContent":"损失电量等效时 (h)",
+            "indexIncrDecrStandard":"0",
+            "indexIncrDecrValue":"0",
+            "indexLowerLimit":"0",
+            "indexName":"损失电量等效时",
+            "indexPercent":"0",
+            "indexTypeCode":"1",
+            "indexTypeName":"发电水平",
+            "indexUnit":"h",
+            "indexUpperLimit":"0",
+            "scoreLevel":null,
+            "scoreValue":null
+        },
+        {
+            "finishStatus":null,
+            "indexCode":"4",
+            "indexContent":"(实际发电量-上网电量+购网电量)/实际发电量*100%",
+            "indexIncrDecrStandard":"1",
+            "indexIncrDecrValue":"2",
+            "indexLowerLimit":"0.01",
+            "indexName":"综合厂用电率",
+            "indexPercent":"10",
+            "indexTypeCode":"1",
+            "indexTypeName":"发电水平",
+            "indexUnit":"%",
+            "indexUpperLimit":"0.01",
+            "scoreLevel":null,
+            "scoreValue":null
+        },
+        {
+            "finishStatus":null,
+            "indexCode":"5",
+            "indexContent":"已消缺数/总故障数*100%",
+            "indexIncrDecrStandard":"1",
+            "indexIncrDecrValue":"2",
+            "indexLowerLimit":"100",
+            "indexName":"消缺率",
+            "indexPercent":"15",
+            "indexTypeCode":"2",
+            "indexTypeName":"运维管理",
+            "indexUnit":"%",
+            "indexUpperLimit":"100",
+            "scoreLevel":null,
+            "scoreValue":null
+        },
+        {
+            "finishStatus":null,
+            "indexCode":"6",
+            "indexContent":"A级故障平均处理时长 (h)",
+            "indexIncrDecrStandard":"24",
+            "indexIncrDecrValue":"20",
+            "indexLowerLimit":"0",
+            "indexName":"消缺及时性",
+            "indexPercent":"5",
+            "indexTypeCode":"2",
+            "indexTypeName":"运维管理",
+            "indexUnit":"h",
+            "indexUpperLimit":"24",
+            "scoreLevel":null,
+            "scoreValue":null
+        },
+        {
+            "finishStatus":null,
+            "indexCode":"7",
+            "indexContent":"B级故障平均处理时长 (h)",
+            "indexIncrDecrStandard":"6",
+            "indexIncrDecrValue":"20",
+            "indexLowerLimit":"0",
+            "indexName":"消缺及时性",
+            "indexPercent":"5",
+            "indexTypeCode":"2",
+            "indexTypeName":"运维管理",
+            "indexUnit":"h",
+            "indexUpperLimit":"6",
+            "scoreLevel":null,
+            "scoreValue":null
+        },
+        {
+            "finishStatus":null,
+            "indexCode":"8",
+            "indexContent":"C级故障平均处理时长 (h)",
+            "indexIncrDecrStandard":"2",
+            "indexIncrDecrValue":"20",
+            "indexLowerLimit":"0",
+            "indexName":"消缺及时性",
+            "indexPercent":"5",
+            "indexTypeCode":"2",
+            "indexTypeName":"运维管理",
+            "indexUnit":"h",
+            "indexUpperLimit":"2",
+            "scoreLevel":null,
+            "scoreValue":null
+        }
+    ]
     const dataSource = indexList.map((e, i) => {
         const judgementStandard=e.indexLowerLimit===e.indexUpperLimit ?e.indexLowerLimit:`${e.indexLowerLimit}~${e.indexUpperLimit}`
+        const indexName=+e.indexCode>5 && e.indexContent || e.indexName;
         return  ({
             ...e,
             key:i,
-            judgementStandard: judgementStandard
+            judgementStandard: judgementStandard,
+            indexName,
         })
     })
 
@@ -25,8 +157,8 @@ export default function table(props) {
                 children: value,
                 props: {},
             };
-            if (index === 0 || index === 4) {
-                obj.props.rowSpan = 3;
+            if (index === 0 ||index === 4) {
+                obj.props.rowSpan = 4;
             }
             if (index === 1 || index === 2 || index === 3 || index === 5 || index === 6) {
                 obj.props.rowSpan = 0;
