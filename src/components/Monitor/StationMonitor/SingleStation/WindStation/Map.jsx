5e67940517022134a51c6a8761872913b70de1ac
haojianling <591191280@qq.com>
2019-03-29 19:51:02 +0800

    地图实时优化

--- src/components/Monitor/StationMonitor/SingleStation/WindStation/Map.jsx
+++ src/components/Monitor/StationMonitor/SingleStation/WindStation/Map.jsx
@@ -29,10 +29,8 @@ class Map extends Component {
 
   componentWillReceiveProps(nextProps) {
     const { testId, stationDataList } = nextProps;
-    if (this.props.stationDataList.length !== nextProps.stationDataList.length) {
-      const testChart = echarts.init(document.getElementById(testId));
-      this.setMapChart(testChart, stationDataList);
-    }
+    const testChart = echarts.init(document.getElementById(testId));
+    this.setMapChart(testChart, stationDataList);
   }
 
   setMapChart = (testChart, stationDataList) => {
