47cca5a94a87eb8996783d3f103a7ba0856cd347
haojianling <591191280@qq.com>
2019-03-29 14:16:58 +0800

    1 MW-kW  2 电站监控进度条的样式

--- src/app.js
+++ src/app.js
@@ -5,18 +5,16 @@ import { Provider } from 'react-redux';
 import {HashRouter} from 'react-router-dom';
 import { LocaleProvider } from 'antd';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import Main from './containers/Main/Main';
 import appReducer from './containers/indexReducer';
 import rootSaga from './containers/indexSaga';
-import logger from 'redux-logger';
 import './theme/reset.scss';
 import './theme/antdReset.scss';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [thunk, sagaMiddleware];
+const middlewares = [sagaMiddleware];
 const store = createStore(appReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
 
