7b93980a380d85cf772abe4de64cd5a5c18b345f
tiandali <tian.chengli@outlook.com>
2019-04-01 13:36:08 +0800

    设备编辑新增需求

--- src/components/System/Station/DeviceManage/AddDevice/AddDeviceForm.jsx
+++ src/components/System/Station/DeviceManage/AddDevice/AddDeviceForm.jsx
@@ -51,8 +51,8 @@ class AddDeviceForm extends Component {
             values.map = {
               assemblyTime: moment(values.assemblyTime).format('YYYY-MM-DD'),
               ongridTime: moment(values.ongridTime).format('YYYY-MM-DD'),
-              warrantyBegintime:moment(values.warrantyBegintime).format('YYYY-MM-DD'),
-              warrantyEndtime:moment(values.warrantyEndtime).format('YYYY-MM-DD'),
+              warrantyBegintime: moment(values.warrantyBegintime).format('YYYY-MM-DD'),
+              warrantyEndtime: moment(values.warrantyEndtime).format('YYYY-MM-DD'),
               scrapTime: moment(values.scrapTime).format('YYYY-MM-DD'),
               hubHeight: values.hubHeight,
               altitude: values.altitude,
@@ -70,7 +70,7 @@ class AddDeviceForm extends Component {
           values.deviceModeCode = addDeviceModeData.deviceModeCode ? `${addDeviceModeData.deviceModeCode}` : `${values.deviceModeCode}`;
           this.props.addDeviceDetail({ ...values })
           this.props.changeDeviceManageStore({ addDeviceTypeData: {}, addDeviceModeData: {} })
-        } 
+        }
       });
     }
   }
@@ -91,7 +91,7 @@ class AddDeviceForm extends Component {
     });
   }
   submitForm = (e) => {
-    this.props.changeDeviceManageStore({deviceNameOk:null})
+    this.props.changeDeviceManageStore({ deviceNameOk: null })
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
@@ -119,24 +119,49 @@ class AddDeviceForm extends Component {
   }
   changeDeviceMode = () => {
   }
+  preConnectDevice = (deviceTypeCode) => {
+    let preDeviceName='';
+    switch (deviceTypeCode) {
+      case '101':
+      preDeviceName= '集电线路'
+      break;
+      case '202':
+      preDeviceName= '逆变器'
+      break;
+      case '304':
+      preDeviceName= '集电线路'
+      break;
+      case '207':
+      preDeviceName= '箱变'
+      break;
+      case '206':
+      preDeviceName= '交流汇流箱'
+      break;
+      case '201':
+      preDeviceName= '箱变'
+      break;
+    }
+    return preDeviceName
+
+  }
   render() {
     const { showAddDeviceModeModal, showAddDeviceMode, deviceModeCodeAdd, manufacturerAdd } = this.state;
     const { getFieldDecorator, getFieldValue } = this.props.form;
-    const { stationDevices, deviceModels, form, selectdeviceType, selectStation, pvDeviceModels, connectDevice, addDeviceTypeData ,addDeviceModeData} = this.props;
+    const { stationDevices, deviceModels, form, selectdeviceType, selectStation, pvDeviceModels, connectDevice, addDeviceTypeData, addDeviceModeData } = this.props;
     const stationName = selectStation ? selectStation[0].stationName : '';
     const deviceTypeName = getFieldValue('deviceTypeCode');
     const deviceModeCodeValue = getFieldValue('deviceModeCode');
     const selectDeviceTypeName = typeof (selectdeviceType) === 'number' ? stationDevices.filter((e, i) => (e.deviceTypeCode === selectdeviceType))[0].deviceTypeName : selectdeviceType
     //101是风电机组，箱变304，测风塔501，组串式逆变器、汇流箱：206、202，集中式逆变器：201
     const modelSelectDisable = deviceModels.length === 0;
-    const initiDeviceMode=addDeviceModeData.data?+addDeviceModeData.data:null;
+    const initiDeviceMode = addDeviceModeData.data ? +addDeviceModeData.data : null;
     const manufacturerArr = deviceModels.filter((e, i) => (e.deviceModeCode === deviceModeCodeValue))[0];
     const manufactureName = manufacturerArr && manufacturerArr.manufacturer;
-    // console.log('selectdeviceType: ', selectdeviceType);
-    const isShow = [ '202', '206', '304', '101','201','207'].includes(`${selectdeviceType}`);//通用的:关联设备，额定，装机，经纬度
-    const isMeteorology = ['203','501',].includes(`${selectdeviceType}`);//测风塔和气象站呈现经纬度
-    const isTemplateMachine = ['201','304','206'].includes(`${selectdeviceType}`);//是否呈现样板机的设备
-    const isShowSaveButton= ['501', '202', '206', '304', '101',].includes(`${selectdeviceType}`);//显示组件
+    // 
+    const isShow = ['202', '206', '304', '101', '201', '207'].includes(`${selectdeviceType}`);//通用的:关联设备，额定，装机，经纬度
+    const isMeteorology = ['203', '501',].includes(`${selectdeviceType}`);//测风塔和气象站呈现经纬度
+    const isTemplateMachine = ['201', '304', '206'].includes(`${selectdeviceType}`);//是否呈现样板机的设备
+    const isShowSaveButton = ['501', '202', '206', '304', '101',].includes(`${selectdeviceType}`);//显示组件
 
 
     return (
@@ -162,35 +187,35 @@ class AddDeviceForm extends Component {
                   <Input placeholder="不超过30字" />
                 )}
               </FormItem>
-            
-                <FormItem label="设备型号" colon={false} className={styles.formItemStyle}>
-                  {getFieldDecorator('deviceModeCode', {
-                    initialValue: initiDeviceMode,
-                    rules: [{ required: true, message: '请选择设备型号' }],
-                  })(
-                    <Select className={styles.modelSelect} placeholder="请选择设备型号" onChange={this.changeDeviceMode} disabled={modelSelectDisable}>
+
+              <FormItem label="设备型号" colon={false} className={styles.formItemStyle}>
+                {getFieldDecorator('deviceModeCode', {
+                  initialValue: initiDeviceMode,
+                  rules: [{ required: true, message: '请选择设备型号' }],
+                })(
+                  <Select className={styles.modelSelect} placeholder="请选择设备型号" onChange={this.changeDeviceMode} disabled={modelSelectDisable}>
                     <Option key={'all'} value={''}>请选择设备设备</Option>
-                      {deviceModels.map((e, i) => {
-                        if (!e) { return null; } else {
-                          return <Option key={e.deviceModeCode} value={e.deviceModeCode}>{e.deviceModeName}</Option>
-                        }
-                      })}
-                    </Select>
-                  )}
-                  <span className={styles.fontColor} onClick={this.showAddDeviceModeModal}>添加设备型号</span>
-                </FormItem>
-            
-                <FormItem label="生产厂家" colon={false} className={styles.formItemStyle}>
-                  {getFieldDecorator('manufacturer', {
-                    initialValue: manufactureName,
-                    rules: [{ required: true, message: '请正确填写', type: "string", max: 30, }],
-                  })(
-                    <Input placeholder="不超过30字" />
-                  )}
-                </FormItem>
-          
+                    {deviceModels.map((e, i) => {
+                      if (!e) { return null; } else {
+                        return <Option key={e.deviceModeCode} value={e.deviceModeCode}>{e.deviceModeName}</Option>
+                      }
+                    })}
+                  </Select>
+                )}
+                <span className={styles.fontColor} onClick={this.showAddDeviceModeModal}>添加设备型号</span>
+              </FormItem>
+
+              <FormItem label="生产厂家" colon={false} className={styles.formItemStyle}>
+                {getFieldDecorator('manufacturer', {
+                  initialValue: manufactureName,
+                  rules: [{ required: true, message: '请正确填写', type: "string", max: 30, }],
+                })(
+                  <Input placeholder="不超过30字" />
+                )}
+              </FormItem>
+
               <FormItem label="批次号" colon={false} className={styles.formItemStyle}>
-                {getFieldDecorator('lotNumber',{
+                {getFieldDecorator('lotNumber', {
                   rules: [{ max: 30, message: '不超过30字' }]
                 })(
                   <Input placeholder="不超过30字" />
@@ -198,10 +223,10 @@ class AddDeviceForm extends Component {
               </FormItem>
             </div>
             <div className={styles.valueStyles}>
-             {isShow && <FormItem label="关联设备" colon={false} className={styles.formItemStyle}>
+              {isShow && <FormItem label="关联设备" colon={false} className={styles.formItemStyle}>
                 {getFieldDecorator('parentDeviceFullcode')(
                   <Select placeholder="请选择关联设备" onChange={this.changeConnect} disabled={connectDevice.length === 0}>
-                  <Option key={'all'} value={''}>请选择关联设备</Option>
+                    <Option key={'all'} value={''}>请选择关联设备</Option>
                     {connectDevice.map((e, i) => {
                       if (!e) { return null; } else {
                         return <Option key={e.deviceFullCode} value={e.deviceFullCode}>{e.deviceName}</Option>
@@ -209,8 +234,9 @@ class AddDeviceForm extends Component {
                     })}
                   </Select>
                 )}
+                <span> {this.preConnectDevice(deviceTypeName+'')}</span>
               </FormItem>}
-             {isTemplateMachine&&<FormItem label="是否为样板机" colon={false} className={styles.formItemStyle}>
+              {isTemplateMachine && <FormItem label="是否为样板机" colon={false} className={styles.formItemStyle}>
                 {getFieldDecorator('templateMachine')(
                   <Select>
                     <Option value={'1'}>是</Option>
@@ -218,26 +244,26 @@ class AddDeviceForm extends Component {
                   </Select>
                 )}
               </FormItem>}
-              {isShow &&<FormItem label="额定容量" colon={false} className={styles.formItemStyle}>
-                {getFieldDecorator('ratedPower',{
-                  rules:[{pattern:/^\d+([.]\d{1,2})?$/,message: '保留小数点后两位'}]
+              {isShow && <FormItem label="额定容量" colon={false} className={styles.formItemStyle}>
+                {getFieldDecorator('ratedPower', {
+                  rules: [{ pattern: /^\d+([.]\d{1,2})?$/, message: '保留小数点后两位' }]
                 })(
                   <Input placeholder="保留小数点后两位" />
                 )}<span className={styles.unitStyle}>kW</span>
               </FormItem>}
-             {isShow && <FormItem label="装机容量" colon={false} className={styles.formItemStyle}>
-                {getFieldDecorator('deviceCapacity',{
-                  rules:[{pattern:/^\d+([.]\d{1,2})?$/,message: '保留小数点后两位'}]
+              {isShow && <FormItem label="装机容量" colon={false} className={styles.formItemStyle}>
+                {getFieldDecorator('deviceCapacity', {
+                  rules: [{ pattern: /^\d+([.]\d{1,2})?$/, message: '保留小数点后两位' }]
                 })(
                   <Input placeholder="保留小数点后两位" />
                 )}<span className={styles.unitStyle}>kW</span>
               </FormItem>}
-             {(isShow||isMeteorology) && <FormItem label="经度" colon={false} className={styles.formItemStyle}>
+              {(isShow || isMeteorology) && <FormItem label="经度" colon={false} className={styles.formItemStyle}>
                 {getFieldDecorator('longitude')(
                   <Input placeholder="请输入..." />
                 )}<span className={styles.unitStyle}>°</span>
               </FormItem>}
-             {(isShow||isMeteorology) && <FormItem label="纬度" colon={false} className={styles.formItemStyle}>
+              {(isShow || isMeteorology) && <FormItem label="纬度" colon={false} className={styles.formItemStyle}>
                 {getFieldDecorator('latitude')(
                   <Input placeholder="请输入..." />
                 )}<span className={styles.unitStyle}>°</span>
@@ -252,29 +278,29 @@ class AddDeviceForm extends Component {
                   </Select>
                 )}
               </FormItem>
-              {!isShowSaveButton&&<div className={styles.leftsubmitStyle}>
-              <Button onClick={this.gobackPre} className={styles.leftpreStyles}>上一步</Button>
-              <Button onClick={this.submitForm} >保存</Button>
-            </div>}
-              </div>
+              {!isShowSaveButton && <div className={styles.leftsubmitStyle}>
+                <Button onClick={this.gobackPre} className={styles.leftpreStyles}>上一步</Button>
+                <Button onClick={this.submitForm} >保存</Button>
+              </div>}
+            </div>
           </div>
           <div className={styles.rightContainer}>
             {deviceTypeName === 501 && <WindMeasurement {...this.props} form={form} />}
             {deviceTypeName === 304 && <div className={styles.rightStyles}>
               <FormItem label="所属方阵" colon={false} className={styles.formItemStyle}>
-                {getFieldDecorator('belongMatrix',{
+                {getFieldDecorator('belongMatrix', {
                   rules: [{ max: 30, message: '不超过30字' }]
                 })(
                   <Input placeholder="不超过30字" />
                 )}
               </FormItem>
             </div>}
-            {deviceTypeName === 101 && <WindInstallDate {...this.props} form={form}  />}
+            {deviceTypeName === 101 && <WindInstallDate {...this.props} form={form} />}
             {(deviceTypeName === 206 || deviceTypeName === 202) && <Confluence {...this.props} selectStation={selectStation[0].stationCode} pvDeviceModels={pvDeviceModels} form={form} />}
-           {isShowSaveButton&& <div className={styles.submitStyle}>
-           <Button onClick={this.gobackPre} className={styles.preStyles}>上一步</Button>
-           <Button onClick={this.submitForm} >保存</Button>
-         </div>}
+            {isShowSaveButton && <div className={styles.submitStyle}>
+              <Button onClick={this.gobackPre} className={styles.preStyles}>上一步</Button>
+              <Button onClick={this.submitForm} >保存</Button>
+            </div>}
           </div>
         </Form>
         {showAddDeviceModeModal && <ShowAddDeviceModeModal {...this.props} showAddDeviceModeModal={showAddDeviceModeModal} cancleDeviceModeModal={this.cancleDeviceModeModal} saveFormState={this.saveFormState} selectdeviceType={selectdeviceType} />}
