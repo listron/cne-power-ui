62dab6277fc5deea65f4161196dff99e6cce68d7
Mr-liu <you@example.com>
2019-04-15 18:22:35 +0800

    :sparkles:

--- src/components/HighAnalysis/FaultDiagnose/FaultSingleFan/FaultSingleFan.jsx
+++ src/components/HighAnalysis/FaultDiagnose/FaultSingleFan/FaultSingleFan.jsx
@@ -5,6 +5,8 @@ import PreTemperature from "../DiagnoseCharts/PreTemperature";
 import AfterTemperature from "../DiagnoseCharts/AfterTemperature";
 import DifferenceTemperature from "../DiagnoseCharts/DifferenceTemperature";
 import SingleResult from "../DiagnoseCharts/SingleResult";
+import HeatMap from "../DiagnoseCharts/HeatMap";
+import AllFans from "../DiagnoseCharts/AllFans";
 
 
 export default class FaultSingleFan extends React.Component {
@@ -27,11 +29,15 @@ export default class FaultSingleFan extends React.Component {
         <div className={styles.title}>
           <span>肥西风场</span><span>WTG05</span>
         </div>
-        <div className={styles.singleFanContent}>
-          <PreTemperature {...this.props} />
-          <AfterTemperature {...this.props} />
-          <DifferenceTemperature {...this.props} />
-          <SingleResult {...this.props} />
+        <div className={styles.singleFanWrap}>
+          <div className={styles.singleFanContent}>
+            <PreTemperature {...this.props} />
+            <AfterTemperature {...this.props} />
+            <DifferenceTemperature {...this.props} />
+            <SingleResult {...this.props} />
+            <HeatMap {...this.props} />
+            <AllFans {...this.props} />
+          </div>
         </div>
       </div>
     );
--- src/utils/chartsConfig/diagnoseConfig.js
+++ src/utils/chartsConfig/diagnoseConfig.js
@@ -1,4 +1,6 @@
 
+const themeColor = "#dfdfdf";
+
 // 故障图表-发电机前驱温度
 export const PreTemperatureOptions = (flag) => {
   var base = +new Date(1968, 9, 3);
@@ -42,6 +44,14 @@ export const PreTemperatureOptions = (flag) => {
     },
     xAxis: {
       type: 'category',
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       boundaryGap: false,
       data: date,
       splitLine: {
@@ -51,6 +61,14 @@ export const PreTemperatureOptions = (flag) => {
     yAxis: {
       type: 'value',
       name: '（。）',
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       splitLine: {
         show: true,
         lineStyle: {
@@ -143,6 +161,14 @@ export const AfterTemperatureOptions = () => {
     },
     xAxis: {
       type: 'category',
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       boundaryGap: false,
       data: date,
       splitLine: {
@@ -152,6 +178,14 @@ export const AfterTemperatureOptions = () => {
     yAxis: {
       type: 'value',
       name: '（。）',
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       splitLine: {
         show: true,
         lineStyle: {
@@ -217,7 +251,7 @@ export const AfterTemperatureOptions = () => {
   };
 };
 
-// 故障图表-发电机后驱温度
+// 故障图表-温度差
 export const diffTemperatureOptions = () => {
   var base = +new Date(1968, 9, 3);
   var oneDay = 24 * 3600 * 1000;
@@ -246,6 +280,14 @@ export const diffTemperatureOptions = () => {
       type: 'category',
       boundaryGap: false,
       data: date,
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       splitLine: {
         show: true,
       }
@@ -253,6 +295,14 @@ export const diffTemperatureOptions = () => {
     yAxis: {
       type: 'value',
       name: '（。）',
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       splitLine: {
         show: true,
         lineStyle: {
@@ -318,7 +368,7 @@ export const diffTemperatureOptions = () => {
   };
 };
 
-// 故障图表-发电机后驱温度
+// 故障图表-单机自适应模块的检测结果
 export const singleTemperatureOptions = () => {
   var base = +new Date(1968, 9, 3);
   var oneDay = 24 * 3600 * 1000;
@@ -345,6 +395,11 @@ export const singleTemperatureOptions = () => {
         fontSize: 12
       }
     },
+    grid: {
+      borderColor: themeColor,
+      borderWidth: 1,
+      show: true
+    },
     tooltip: {
       trigger: 'axis',
       position: function (pt) {
@@ -354,13 +409,30 @@ export const singleTemperatureOptions = () => {
     xAxis: {
       type: 'category',
       boundaryGap: false,
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       data: date,
     },
     yAxis: {
       type: 'value',
       name: '（。）',
+      axisLine:{
+        lineStyle:{
+          width: 0, //这里是为了突出显示加上的
+        }
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
       splitLine: {
         show: false,
+        color: themeColor
       },
       boundaryGap: [0, '100%']
     },
@@ -394,6 +466,147 @@ export const singleTemperatureOptions = () => {
         },
         data:[120, 132, 101, 134, 90, 230, 210]
       },
+    ]
+  };
+};
+
+// 故障图表-相似性热图
+export const heatTemperatureOptions = () => {
+  var hours = ['12a', '1a', '2a', '3a', '4a', '5a', '6a',
+    '7a', '8a', '9a','10a','11a',
+    '12p', '1p', '2p', '3p', '4p', '5p',
+    '6p', '7p', '8p', '9p', '10p', '11p'];
+  var days = ['Saturday', 'Friday', 'Thursday',
+    'Wednesday', 'Tuesday', 'Monday', 'Sunday'];
+
+  var data = [[0,0,5],[0,1,1],[0,2,0],[0,3,0],[0,4,0],[0,5,0],[0,6,0],[0,7,0],[0,8,0],[0,9,0],[0,10,0],[0,11,2],[0,12,4],[0,13,1],[0,14,1],[0,15,3],[0,16,4],[0,17,6],[0,18,4],[0,19,4],[0,20,3],[0,21,3],[0,22,2],[0,23,5],[1,0,7],[1,1,0],[1,2,0],[1,3,0],[1,4,0],[1,5,0],[1,6,0],[1,7,0],[1,8,0],[1,9,0],[1,10,5],[1,11,2],[1,12,2],[1,13,6],[1,14,9],[1,15,11],[1,16,6],[1,17,7],[1,18,8],[1,19,12],[1,20,5],[1,21,5],[1,22,7],[1,23,2],[2,0,1],[2,1,1],[2,2,0],[2,3,0],[2,4,0],[2,5,0],[2,6,0],[2,7,0],[2,8,0],[2,9,0],[2,10,3],[2,11,2],[2,12,1],[2,13,9],[2,14,8],[2,15,10],[2,16,6],[2,17,5],[2,18,5],[2,19,5],[2,20,7],[2,21,4],[2,22,2],[2,23,4],[3,0,7],[3,1,3],[3,2,0],[3,3,0],[3,4,0],[3,5,0],[3,6,0],[3,7,0],[3,8,1],[3,9,0],[3,10,5],[3,11,4],[3,12,7],[3,13,14],[3,14,13],[3,15,12],[3,16,9],[3,17,5],[3,18,5],[3,19,10],[3,20,6],[3,21,4],[3,22,4],[3,23,1],[4,0,1],[4,1,3],[4,2,0],[4,3,0],[4,4,0],[4,5,1],[4,6,0],[4,7,0],[4,8,0],[4,9,2],[4,10,4],[4,11,4],[4,12,2],[4,13,4],[4,14,4],[4,15,14],[4,16,12],[4,17,1],[4,18,8],[4,19,5],[4,20,3],[4,21,7],[4,22,3],[4,23,0],[5,0,2],[5,1,1],[5,2,0],[5,3,3],[5,4,0],[5,5,0],[5,6,0],[5,7,0],[5,8,2],[5,9,0],[5,10,4],[5,11,1],[5,12,5],[5,13,10],[5,14,5],[5,15,7],[5,16,11],[5,17,6],[5,18,0],[5,19,5],[5,20,3],[5,21,4],[5,22,2],[5,23,0],[6,0,1],[6,1,0],[6,2,0],[6,3,0],[6,4,0],[6,5,0],[6,6,0],[6,7,0],[6,8,0],[6,9,0],[6,10,1],[6,11,0],[6,12,2],[6,13,1],[6,14,3],[6,15,4],[6,16,0],[6,17,0],[6,18,0],[6,19,0],[6,20,1],[6,21,2],[6,22,2],[6,23,6]];
+
+  data = data.map(function (item) {
+    return [item[1], item[0], item[2] || '-'];
+  });
+  return {
+    tooltip: {
+      position: 'top'
+    },
+    animation: false,
+    grid: {
+      height: '80%',
+      y: '10%'
+    },
+    xAxis: {
+      type: 'category',
+      data: hours,
+      splitArea: {
+        show: true
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
+    },
+    yAxis: {
+      type: 'category',
+      data: days,
+      splitArea: {
+        show: true
+      },
+      axisTick:{
+        lineStyle:{ color: themeColor}    // 刻度的颜色
+      },
+    },
+    visualMap: {
+      min: 0,
+      max: 10,
+      calculable: true,
+      orient: 'vertical',
+      top: "5%",
+      right: "5%"
+    },
+    series: [{
+      name: 'Punch Card',
+      type: 'heatmap',
+      data: data,
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }]
+  };
+};
+
+// 故障图表-严重程度及识别（所有风机）
+export const allFansOptions = () => {
+  return {
+    color: ['#3398DB'],
+    tooltip : {
+      trigger: 'axis',
+      axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      borderColor: themeColor,
+      borderWidth: 1,
+      show: true,
+      containLabel: true
+    },
+    xAxis : [
+      {
+        type : 'category',
+        axisLine:{
+          lineStyle:{
+            width: 0, //这里是为了突出显示加上的
+          }
+        },
+        data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisTick: {
+          alignWithLabel: true,
+          lineStyle:{ color: themeColor}    // 刻度的颜色
+        }
+      }
+    ],
+    yAxis : [
+      {
+        type : 'value',
+        axisLine:{
+          lineStyle:{
+            width: 0, //这里是为了突出显示加上的
+          }
+        },
+          splitLine: {
+          show: false,
+          color: themeColor
+        },
+        axisTick: {
+          lineStyle:{ color: themeColor}    // 刻度的颜色
+        }
+      }
+    ],
+    series : [
+      {
+        name:'直接访问',
+        type:'bar',
+        barWidth: '60%',
+        data:[10, 52, 200, 334, 390, 330, 220]
+      },
+      {
+        name:'直接访问',
+        type:'line',
+        barWidth: '60%',
+        itemStyle : {
+          normal : {
+            color:'yellow',
+          }
+        },
+        lineStyle: {
+          type: "dashed"
+        },
+        data:[5, 14, 20, 104, 230, 110, 20]
+      }
     ]
   };
 };
