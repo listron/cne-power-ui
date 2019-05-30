e5b3c7fb49cf9c17018f1dc4aa8a930bf9de2d21
Mr-liu <you@example.com>
2019-05-30 17:54:25 +0800

    :sparkles: 设备台账

--- src/containers/Operation/Book/DeviceAccount/deviceAccountSaga.js
+++ src/containers/Operation/Book/DeviceAccount/deviceAccountSaga.js
@@ -1,57 +1,173 @@
-import { call, put, takeLatest, select } from 'redux-saga/effects';
-import axios from 'axios';
-import { message } from 'antd';
-import Path from '../../../../constants/path';
+import {put, takeEvery, call} from 'redux-saga/effects';
 import { deviceAccountAction } from './deviceAccountAction';
-import moment from 'moment';
-const APIBasePath=Path.basePaths.APIBasePath;
-const monitor=Path.APISubPaths.monitor
+import Path from "../../../../constants/path";
+import axios from "axios";
 
-// function *getPowerReportList(action) {  // 请求报表列表
-//   const { payload } = action;
-//   const url =`${APIBasePath}${monitor.getPowerReportList}`;
-//   // const url =`/mock/v3/wind/report/fan/gen`;
+/***
+ * 解析公共头APIBasePath
+ * operation下面的接口
+ */
 
-//   try{
-//     yield put({
-//       type:deviceAccountAction.changedeviceAccountStore,
-//       payload: {
-//         loading: true,
-//       },
-//     });  
-//     const response = yield call(axios.post,url,{
-//       ...payload,
-//     });
-//     if(response.data.code === '10000') {
-//       const total = response.data.data.pageCount || 0;
-//       let { pageNum, pageSize } = payload;
-//       const maxPage = Math.ceil(total / pageSize);
-//       if (total === 0) { // 总数为0时，展示0页
-//         pageNum = 1;
-//       } else if (maxPage < pageNum) { // 当前页已超出
-//         pageNum = maxPage;
-//       }
-//       yield put({
-//         type:deviceAccountAction.changedeviceAccountStore,
-//         payload: {
-//           total:response.data.data.pageCount||0,
-//           loading:false,
-//           ...payload,
-//         },
-//       });     
-//     }else{
-//       throw response.data
-//     }  
-//   }catch(e){
-//     console.log(e);
-//     yield put({
-//       type:deviceAccountAction.changedeviceAccountStore,
-//       payload: { ...payload, loading: false ,powerReportList:[]},
-//     })
-//   }
-// }
+const {
+  basePaths: {
+    APIBasePath,
+  },
+  APISubPaths: {
+    operation: {
+      deviceAccountList,
+      stationsManufactors,
+      deviceModeList,
+      attachmentsList
+    }
+  }
+} = Path;
+
+function* getDeviceAccountList(action) { // 设备台账列表
+  const { payload } = action;
+  const url = `${APIBasePath}${deviceAccountList}`;
+  try {
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: true,
+        deviceAccountListLoading: true,
+        ...payload
+      }
+    });
+    const response = yield call(axios.post, url, payload);
+    if (response.data.code === '10000') {
+      yield put({
+        type: deviceAccountAction.deviceAccountFetchSuccess,
+        payload: {
+          deviceAccountList: response.data.data || {},
+          loading: false,
+          deviceAccountListLoading: false,
+        },
+      });
+    }else {
+      throw response.data
+    }
+  } catch (e) {
+    console.log(e);
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: false,
+        deviceAccountListLoading: false,
+      }
+    });
+  }
+}
+
+function* getStationsManufactorsList(action) { // 获取电站下的厂家列表
+  const { payload } = action;
+  const url = `${APIBasePath}${stationsManufactors}`;
+  try {
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: true,
+      }
+    });
+    const response = yield call(axios.post, url, payload);
+    if (response.data.code === '10000') {
+      yield put({
+        type: deviceAccountAction.deviceAccountFetchSuccess,
+        payload: {
+          stationsManufactorsList: response.data.data || [],
+          loading: false,
+        },
+      });
+    }else {
+      throw response.data
+    }
+  } catch (e) {
+    console.log(e);
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: false,
+      }
+    });
+  }
+}
+
+function* getDeviceModeList(action) { // 获取厂家下的设备型号列表
+  const { payload } = action;
+  const url = `${APIBasePath}${deviceModeList}/${payload.manufactorId}`;
+  try {
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: true,
+      }
+    });
+    const response = yield call(axios.get, url);
+    if (response.data.code === '10000') {
+      yield put({
+        type: deviceAccountAction.deviceAccountFetchSuccess,
+        payload: {
+          deviceModeList: response.data.data || [],
+          loading: false,
+        },
+      });
+    }else {
+      throw response.data
+    }
+  } catch (e) {
+    console.log(e);
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: false,
+      }
+    });
+  }
+}
+
+function* getDeviceAttachments(action) { // 台账备件列表
+  const { payload } = action;
+  const url = `${APIBasePath}${attachmentsList}`;
+  try {
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: true,
+        modeIdDetails: payload.modeId,
+        assetsIdDetails: payload.assetsId,
+        orderFieldDetails: payload.orderField,
+        orderMethodDetails: payload.orderMethod,
+        pageNumDetails: payload.pageNum,
+        pageSizeDetails: payload.pageSize
+      }
+    });
+    const response = yield call(axios.post, url, payload);
+    if (response.data.code === '10000') {
+      yield put({
+        type: deviceAccountAction.deviceAccountFetchSuccess,
+        payload: {
+          attachmentsList: response.data.data || {},
+          loading: false,
+        },
+      });
+    }else {
+      throw response.data
+    }
+  } catch (e) {
+    console.log(e);
+    yield put({
+      type: deviceAccountAction.deviceAccountFetchSuccess,
+      payload: {
+        loading: false,
+      }
+    });
+  }
+}
 
 
 export function* watchDeviceAccount() {
-  // yield takeLatest(deviceAccountAction.getPowerReportList, getPowerReportList);
+  yield takeEvery(deviceAccountAction.getDeviceAccountList, getDeviceAccountList);
+  yield takeEvery(deviceAccountAction.getStationsManufactorsList, getStationsManufactorsList);
+  yield takeEvery(deviceAccountAction.getDeviceModeList, getDeviceModeList);
+  yield takeEvery(deviceAccountAction.getDeviceAttachments, getDeviceAttachments);
 }
