2c8d0a0c15cf51e71f3f6c8d865bf454c074e36a
haojianling <591191280@qq.com>
2019-06-27 18:32:46 +0800

    日报下载增加loading

--- src/containers/StatisticalAnalysis/StatisticalReport/GeneralReport/GeneralReport.jsx
+++ src/containers/StatisticalAnalysis/StatisticalReport/GeneralReport/GeneralReport.jsx
@@ -1,7 +1,7 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Icon, DatePicker, Button, message } from "antd";
-import styles from  './generalReport.scss';
+import styles from './generalReport.scss';
 import CommonBreadcrumb from "../../../../components/Common/CommonBreadcrumb";
 import StationSelect from '../../../../components/Common/StationSelect';
 import Footer from "../../../../components/Common/Footer";
@@ -12,48 +12,49 @@ import Cookie from 'js-cookie';
 import { enterpriseKey } from '../../../../constants/enterpriseKey';
 
 const { APIBasePath } = path.basePaths;
-const { 
+const {
   dailyreport, faultReport, genReport, indicatorReport
 } = path.APISubPaths.statisticalAnalysis;
 const { MonthPicker } = DatePicker;
 
-class GeneralReport extends Component{
-  constructor(props){
+class GeneralReport extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      reportDate : moment().subtract(1,'day'),
-      faultDate : moment().subtract(1,'day'),
-      eleInfoDate : moment().subtract(1,'day'),
-      proOperationDate : moment().subtract(1,'month'),
-      selectedStation: []
+      reportDate: moment().subtract(1, 'day'),
+      faultDate: moment().subtract(1, 'day'),
+      eleInfoDate: moment().subtract(1, 'day'),
+      proOperationDate: moment().subtract(1, 'month'),
+      selectedStation: [],
+      typeDowning: '', // report  fault eleInfo proOperate
     }
   }
 
   ChangeReportDate = value => { // 日报
     this.setState({
-      reportDate : value
-    })
-  }
- 
-  ChangeFaultDate = value => { //故障日报
-    this.setState({
-      faultDate : value
-    })
-  }
-  
-  ChangeEleInfoDate = value =>{//发电量信息汇总
-    this.setState({
-      eleInfoDate : value
-    })
-  }
- 
-  ChangeProOperationDate = value => { // 生产指标运行
-    this.setState({
-      proOperationDate : value
+      reportDate: value
     })
   }
 
-  
+  ChangeFaultDate = value => { //故障日报
+    this.setState({
+      faultDate: value
+    })
+  }
+
+  ChangeEleInfoDate = value => {//发电量信息汇总
+    this.setState({
+      eleInfoDate: value
+    })
+  }
+
+  ChangeProOperationDate = value => { // 生产指标运行
+    this.setState({
+      proOperationDate: value
+    })
+  }
+
+
   disabledDate = (current) => { //日期不可选
     return current && current > moment().startOf('day');
   }
@@ -63,51 +64,63 @@ class GeneralReport extends Component{
   };
 
   downloadReport = () => { // 日报下载
-    let { reportDate } = this.state;
+    let { reportDate, typeDowning } = this.state;
     reportDate = reportDate.format('YYYY-MM-DD');
     const downloadHref = `${APIBasePath}/${dailyreport}/${reportDate}`;
     const fileName = `${reportDate}日报.xlsx`;
-    this.downLoadFun(downloadHref, fileName, reportDate);
+    this.setState({ typeDowning: 'report' })
+    if (typeDowning !== 'report') {
+      this.downLoadFun(downloadHref, fileName, reportDate);
+    }
   }
 
   downloadFault = () => { // 故障日报
-    let { faultDate } = this.state;
+    let { faultDate, typeDowning } = this.state;
     faultDate = faultDate.format('YYYY-MM-DD');
     const downloadHref = `${APIBasePath}/${faultReport}/${faultDate}`;
     const fileName = `${faultDate}故障日报.xlsx`;
-    this.downLoadFun(downloadHref, fileName, faultDate);
+    this.setState({ typeDowning: 'fault' })
+    if (typeDowning !== 'fault') {
+      this.downLoadFun(downloadHref, fileName, faultDate);
+    }
   }
 
   downloadGenInfo = () => { // 发电量信息下载
-    let { eleInfoDate } = this.state;
+    let { eleInfoDate, typeDowning } = this.state;
     eleInfoDate = eleInfoDate.format('YYYY-MM-DD');
     const downloadHref = `${APIBasePath}/${genReport}/${eleInfoDate}`;
     const fileName = `${eleInfoDate}发电量信息汇总.xlsx`;
-    this.downLoadFun(downloadHref, fileName, eleInfoDate);
+    this.setState({ typeDowning: 'eleInfo' })
+    if (typeDowning !== 'eleInfo') {
+      this.downLoadFun(downloadHref, fileName, eleInfoDate);
+    }
   }
 
   downloadIndicator = () => { // 生产运营指标下载
-    let { proOperationDate, selectedStation } = this.state;
+    let { proOperationDate, selectedStation,typeDowning } = this.state;
     const { stationCode } = selectedStation[0];
     const indicatorYear = proOperationDate.format('YYYY');
     const indicatorMonth = proOperationDate.format('MM');
     const downloadHref = `${APIBasePath}/${indicatorReport}/${stationCode}/${indicatorYear}/${indicatorMonth}`;
     const fileName = `${proOperationDate}生产指标运行.xlsx`;
-    this.downLoadFun(downloadHref, fileName, proOperationDate);
+    this.setState({ typeDowning: 'proOperate' })
+    if (typeDowning !== 'proOperate') {
+      this.downLoadFun(downloadHref, fileName, proOperationDate);
+    }
   }
 
   downLoadFun = (url, fileName, date) => { // 根据路径，名称，日期，通用下载函数。
-    axios.post(url,{},{responseType:'blob'}).then(response=>{
+    axios.post(url, {}, { responseType: 'blob' }).then(response => {
       const fileContent = response.data;
       const fileNameInfo = response.headers['content-disposition'];
       let newFileName = fileName;
-      if(fileNameInfo){
+      if (fileNameInfo) {
         const fileString = fileNameInfo.split(';')[1];
-        const fileNameCode = fileString? fileString.split('=')[1]: '';
-        const fileResult = fileNameCode?decodeURIComponent(fileNameCode): '';
+        const fileNameCode = fileString ? fileString.split('=')[1] : '';
+        const fileResult = fileNameCode ? decodeURIComponent(fileNameCode) : '';
         fileResult && (newFileName = fileResult)
       }
-      if(fileContent) {
+      if (fileContent) {
         const blob = new Blob([fileContent]);
         if ('download' in document.createElement('a')) { // 非IE下载
           const elink = document.createElement('a');
@@ -118,102 +131,105 @@ class GeneralReport extends Component{
           elink.click();
           URL.revokeObjectURL(elink.href); // 释放URL 对象
           document.body.removeChild(elink);
+          this.setState({ typeDowning: '' })
         } else { // IE10+下载
           navigator.msSaveBlob(blob, newFileName);
-        }   
+        }
+
       }
-    }).catch(warning=>{
+    }).catch(warning => {
       message.warning(`下载失败！请重新尝试`)
       console.log(warning)
     })
   }
-  
-  render(){
+
+  render() {
     const {
-      reportDate, faultDate, eleInfoDate, proOperationDate, selectedStation
+      reportDate, faultDate, eleInfoDate, proOperationDate, selectedStation, typeDowning
     } = this.state;
     const { stations } = this.props;
     const enterpriseName = Cookie.get('enterpriseName');
     const reportInfo = enterpriseKey.find(e => e.enterpriseName === enterpriseName);
-    return(
+    return (
       <div className={styles.generalReportBox}>
-        <CommonBreadcrumb breadData={{name:'通用报表'}} style={{marginLeft:'38px'}}></CommonBreadcrumb>
+        <CommonBreadcrumb breadData={{ name: '通用报表' }} style={{ marginLeft: '38px' }}></CommonBreadcrumb>
         <div className={styles.generalReportContainer}>
           <div className={styles.generalReportMain}>
             <div className={styles.dailyBox}>
               <div className={styles.boxTop}>
-                  <div className={styles.dayReport}>
-                    <Icon type="download" style={{color:'#ffffff'}} />
-                  </div>
-                  <span className={styles.title}>日报</span>
+                <div className={styles.dayReport}>
+                  <Icon type="download" style={{ color: '#ffffff' }} />
+                </div>
+                <span className={styles.title}>日报</span>
               </div>
               <div className={styles.dateSearch}>
-                <DatePicker 
-                disabledDate={this.disabledDate}
-                placeholder={'选择时间'} 
-                onChange={this.ChangeReportDate} 
-                value={reportDate} 
+                <DatePicker
+                  disabledDate={this.disabledDate}
+                  placeholder={'选择时间'}
+                  onChange={this.ChangeReportDate}
+                  value={reportDate}
                 />
               </div>
               <div className={styles.downloadBtn}>
-                <Button className={styles.text} onClick={this.downloadReport} disabled={!reportDate}>下载</Button>
+                <Button className={styles.text} onClick={this.downloadReport} disabled={!reportDate}
+                  loading={typeDowning === 'report'}>下载</Button>
               </div>
             </div>
             {reportInfo && reportInfo.showAllReport && <div className={styles.dailyBox}>
               <div className={styles.boxTop}>
                 <div className={styles.defaultReport}>
-                  <Icon type="download" style={{color:'#ffffff'}} />
+                  <Icon type="download" style={{ color: '#ffffff' }} />
                 </div>
                 <span className={styles.title}>故障日报</span>
               </div>
               <div className={styles.dateSearch}>
-                <DatePicker 
-                disabledDate={this.disabledDate}
-                placeholder={'选择时间'}
-                onChange={this.ChangeFaultDate} 
-                value={faultDate} 
-                />
+                <DatePicker
+                  disabledDate={this.disabledDate}
+                  placeholder={'选择时间'}
+                  onChange={this.ChangeFaultDate}
+                  value={faultDate}
+                /> 
               </div>
               <div className={styles.downloadBtn}>
-                <Button disabled={!faultDate} onClick={this.downloadFault} className={styles.text}>下载</Button>
+                <Button disabled={!faultDate} onClick={this.downloadFault} className={styles.text} loading={typeDowning === 'fault'}>下载</Button>
               </div>
             </div>}
             {reportInfo && reportInfo.showAllReport && <div className={styles.dailyBox}>
               <div className={styles.boxTop}>
                 <div className={styles.eleInfo}>
-                  <Icon type="download" style={{color:'#ffffff'}} />
+                  <Icon type="download" style={{ color: '#ffffff' }} />
                 </div>
                 <span className={styles.title}>发电量信息汇总</span>
               </div>
               <div className={styles.dateSearch}>
-                <DatePicker 
+                <DatePicker
                   disabledDate={this.disabledDate}
                   placeholder={'选择时间'}
                   onChange={this.ChangeEleInfoDate}
-                  value={eleInfoDate} 
+                  value={eleInfoDate}
                 />
-              </div>
+              </div> 
               <div className={styles.downloadBtn}>
-                <Button disabled={!eleInfoDate} className={styles.text} onClick={this.downloadGenInfo}>下载</Button>
+                <Button disabled={!eleInfoDate} className={styles.text} onClick={this.downloadGenInfo} loading={typeDowning === 'eleInfo'}>下载</Button>
               </div>
             </div>}
             {reportInfo && reportInfo.showAllReport && <div className={styles.dailyBox}>
               <div className={styles.boxTop}>
                 <div className={styles.proOperation}>
-                    <Icon type="download" style={{color:'#ffffff'}} />
+                  <Icon type="download" style={{ color: '#ffffff' }} />
                 </div>
                 <span className={styles.title}>生产指标运行</span>
               </div>
               <div className={styles.dateSearch}>
-                <MonthPicker 
+                <MonthPicker
                   disabledDate={this.disabledDate}
-                  placeholder={'选择月份'} 
+                  placeholder={'选择月份'}
                   onChange={this.ChangeProOperationDate}
                   value={proOperationDate}
                 />
               </div>
               <div className={styles.stationSearchs}>
-                <StationSelect 
+                <StationSelect
                   data={stations}
                   onOK={this.selectStation}
                   value={selectedStation}
@@ -222,9 +238,10 @@ class GeneralReport extends Component{
               </div>
               <div className={styles.downloadBtn}>
                 <Button
-                  disabled={ selectedStation.length === 0 || !proOperationDate}
+                  disabled={selectedStation.length === 0 || !proOperationDate}
                   className={styles.text}
                   onClick={this.downloadIndicator}
+                  loading={typeDowning === 'proOperate'}
                 >下载</Button>
               </div>
             </div>}
