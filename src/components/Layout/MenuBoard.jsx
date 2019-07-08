8a252dea15448d7d906c97b653d604351b78c70d
haojianling <591191280@qq.com>
2019-07-08 10:08:33 +0800

    修改操作票的bug

--- src/components/Layout/MenuBoard.jsx
+++ src/components/Layout/MenuBoard.jsx
@@ -6,7 +6,7 @@ import { withRouter } from 'react-router-dom';
 import { Modal } from 'antd';
 import styles from './layout.scss';
 
-class MenuBoard extends Component{
+class MenuBoard extends Component {
 
   static propTypes = {
     screenAddress: PropTypes.string,
@@ -19,22 +19,22 @@ class MenuBoard extends Component{
   state = {
     rightMenu: localStorage.getItem('rightMenu') ? localStorage.getItem('rightMenu').split(',') : [], // 菜单权限
     linkInfo: [
-      { text: '实时监控', path: '/monitor/station', right: 'monitor_station', icon: 'icon-monitoring',  type: 'long', color: '#2b79c9'},
-      { text: '工单', path: '/operation/ticket/list', right: 'operation_worklist_work', icon: 'icon-gd2',  type: 'short', color: '#bc1d4b'},
-      { text: '两票', path: '/operation/twoTickets/typeone', right: 'operation_twoTicket', icon: 'iconfont icon-gd1',  type: 'short', color: '#2159c2'},
-      { text: '台账', path: '/operation/book/assetsConfig', right: 'operation_book_assetConfig', icon: 'icon-gd3',  type: 'long', color: '#2e9a00'},
+      { text: '实时监控', path: '/monitor/station', right: 'monitor_station', icon: 'icon-monitoring', type: 'long', color: '#2b79c9' },
+      { text: '工单', path: '/operation/ticket/list', right: 'operation_worklist_work', icon: 'icon-gd2', type: 'short', color: '#bc1d4b' },
+      { text: '两票', path: '/operation/twoTickets/workflow', right: 'operation_twoTicket', icon: 'iconfont icon-gd1', type: 'short', color: '#2159c2' },
+      { text: '台账', path: '/operation/book/assetsConfig', right: 'operation_book_assetConfig', icon: 'icon-gd3', type: 'long', color: '#2e9a00' },
 
-      { text: '日报', path: '/operation/running/dayReport', right: 'operation_running_daily', icon: 'icon-running',  type: 'short', color: '#623ebe'}, // 核对icon
-      { text: '统计分析', path: '/statistical/stationaccount/allstation', right: 'statistics_station_all', icon: 'icon-station-data',  type: 'short', color: '#ba204d'},
-      { text: '智能专家库', path: '/operation/intelligentExpert', right: 'operation_experience', icon: 'icon-gd4',  type: 'long', color: '#d49f04'},
-      { text: '运维管理', path: '/operation/gps', right: 'operation_locate', icon: 'icon-control',  type: 'short', color: '#42bcf4'},
-      { text: '高级分析', path: '/analysis/intelligentWarning/realtime', right: 'analysis_intelligentWarning_pending', icon: 'icon-da',  type: 'short', color: '#da5534'},
+      { text: '日报', path: '/operation/running/dayReport', right: 'operation_running_daily', icon: 'icon-running', type: 'short', color: '#623ebe' }, // 核对icon
+      { text: '统计分析', path: '/statistical/stationaccount/allstation', right: 'statistics_station_all', icon: 'icon-station-data', type: 'short', color: '#ba204d' },
+      { text: '智能专家库', path: '/operation/intelligentExpert', right: 'operation_experience', icon: 'icon-gd4', type: 'long', color: '#d49f04' },
+      { text: '运维管理', path: '/operation/gps', right: 'operation_locate', icon: 'icon-control', type: 'short', color: '#42bcf4' },
+      { text: '高级分析', path: '/analysis/intelligentWarning/realtime', right: 'analysis_intelligentWarning_pending', icon: 'icon-da', type: 'short', color: '#da5534' },
 
-      { text: '监控大屏', icon: 'icon-screen', type: 'long', open: true, color: '#199475'}, // 新开大屏网页
-      { text: '统计报表', path: '/statistical/statement/currency', right: 'statistics_report_general', icon: 'icon-count',  type: 'short', color: '#298092'},
-      { text: '主页', path: '/homepage', icon: 'icon-gohome', right: 'homepage',  type: 'short', color: '#525252', },
-      { text: '系统管理', path: '/system/account/enterprise', right: 'system_account_enterprise', icon: 'icon-goset',  type: 'long', color: '#2d8ab4'},
-    ]
+      { text: '监控大屏', icon: 'icon-screen', type: 'long', open: true, color: '#199475' }, // 新开大屏网页
+      { text: '统计报表', path: '/statistical/statement/currency', right: 'statistics_report_general', icon: 'icon-count', type: 'short', color: '#298092' },
+      { text: '主页', path: '/homepage', icon: 'icon-gohome', right: 'homepage', type: 'short', color: '#525252' },
+      { text: '系统管理', path: '/system/account/enterprise', right: 'system_account_enterprise', icon: 'icon-goset', type: 'long', color: '#2d8ab4' },
+    ],
   }
 
   showBoard = () => {
@@ -44,7 +44,7 @@ class MenuBoard extends Component{
   hideBoard = () => {
     this.props.changeCommonStore({ menuBoardShow: false });
   }
-  
+
   toggleBoard = () => {
     this.props.menuBoardShow ? this.hideBoard() : this.showBoard();
   }
@@ -69,14 +69,14 @@ class MenuBoard extends Component{
       onClick={() => this.jumpToPage(e)}
       style={{
         backgroundColor: e.color,
-        cursor: (e.open || this.state.rightMenu.includes(e.right)) ? 'pointer' : 'not-allowed'
+        cursor: (e.open || this.state.rightMenu.includes(e.right)) ? 'pointer' : 'not-allowed',
       }}>
       <span className={`iconfont ${e.icon} ${styles.icon}`} />
       <span className={styles.text}>{e.text}</span>
     </div>
   ))
 
-  render(){
+  render() {
     const { linkInfo } = this.state;
     const { menuBoardShow, menuBoardRequired } = this.props;
     const enterpriseId = Cookie.get('enterpriseId');
@@ -110,7 +110,7 @@ class MenuBoard extends Component{
           </div>
         </Modal>
       </div>
-    )
+    );
   }
 }
 
