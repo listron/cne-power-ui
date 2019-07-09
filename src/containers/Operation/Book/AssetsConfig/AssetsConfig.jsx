7769e7d3ea79816707bf2b822ea9dd1997721efc
tiandali <tian.chengli@outlook.com>
2019-07-09 17:52:06 +0800

    修改分页

--- src/containers/Operation/Book/AssetsConfig/AssetsConfig.jsx
+++ src/containers/Operation/Book/AssetsConfig/AssetsConfig.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import styles from "./assetsConfig.scss";
+import styles from './assetsConfig.scss';
 import CommonBreadcrumb from '../../../../components/Common/CommonBreadcrumb';
 import { assetConfigAction } from './assetsConfigAction';
 import { commonAction } from '../../../../containers/alphaRedux/commonAction';
@@ -19,15 +19,15 @@ class AssetsConfig extends Component {
 
   }
   constructor(props, context) {
-    super(props, context)
+    super(props, context);
   }
   componentDidMount() {
 
   }
 
   queryTargetData = (activeKey) => { //头部tab切换
-    const { changeAssetConfigStore, } = this.props;
-    changeAssetConfigStore({ selectType: activeKey, });
+    const { changeAssetConfigStore } = this.props;
+    changeAssetConfigStore({ selectType: activeKey });
   }
 
   render() {
@@ -36,18 +36,18 @@ class AssetsConfig extends Component {
       breadData: [
         {
           name: '资产配置',
-        }
+        },
       ],
     };
     return (
       <div className={styles.containerDiv}>
-        <CommonBreadcrumb  {...breadCrumbData} style={{ marginLeft: '38px' }} />
+        <CommonBreadcrumb {...breadCrumbData} style={{ marginLeft: '38px' }} />
         <div className={styles.containerBg}>
           <div className={styles.container}>
             <div className={styles.allStationTitle} >
-              <p className={selectType === 'assetStructure' ? styles.activeStation : ''} onClick={() => { this.queryTargetData('assetStructure') }}>资产结构</p>
-              <p className={selectType === 'deviceFactory' ? styles.activeStation : ''} onClick={() => { this.queryTargetData('deviceFactory') }}>设备厂家</p>
-              <p className={selectType === 'deviceMode' ? styles.activeStation : ''} onClick={() => { this.queryTargetData('deviceMode') }}>设备型号</p>
+              <p className={selectType === 'assetStructure' ? styles.activeStation : ''} onClick={() => { this.queryTargetData('assetStructure'); }}>资产结构</p>
+              <p className={selectType === 'deviceFactory' ? styles.activeStation : ''} onClick={() => { this.queryTargetData('deviceFactory'); }}>设备厂家</p>
+              <p className={selectType === 'deviceMode' ? styles.activeStation : ''} onClick={() => { this.queryTargetData('deviceMode'); }}>设备型号</p>
             </div>
             {selectType === 'assetStructure' && <AssetStructure {...this.props} />}
             {selectType === 'deviceFactory' && <DeviceFactory {...this.props} />}
@@ -56,7 +56,7 @@ class AssetsConfig extends Component {
         </div>
         <Footer />
       </div>
-    )
+    );
   }
 }
 const mapStateToProps = (state) => {
@@ -66,24 +66,24 @@ const mapStateToProps = (state) => {
     deviceTypes: state.common.get('deviceTypes').toJS(),
     stationTypeCount: state.common.get('stationTypeCount'),
 
-  }
-}
+  };
+};
 const mapDispatchToProps = (dispatch) => ({
   changeAssetConfigStore: payload => dispatch({ type: assetConfigAction.changeAssetConfigStore, payload }),
   resetAssetConfigStore: payload => dispatch({ type: assetConfigAction.resetAssetConfigStore, payload }),
-  getAssetTree: payload => dispatch({ type: assetConfigAction.getAssetTree, payload }),// 台账生产资产树
-  getNodeDetail: payload => dispatch({ type: assetConfigAction.getNodeDetail, payload }),// 台账生产资产树节点详情
-  addAssetNode: payload => dispatch({ type: assetConfigAction.addAssetNode, payload }),//台账增加生产资产节点
-  deleteAssetNode: payload => dispatch({ type: assetConfigAction.deleteAssetNode, payload }),//台账删除生产资产树
-  editAssetNode: payload => dispatch({ type: assetConfigAction.editAssetNode, payload }),//台账编辑生产资产节点
-  getDeviceFactorsList: payload => dispatch({ type: assetConfigAction.getDeviceFactorsList, payload }),//获取设备厂家列表
-  addDeviceFactors: payload => dispatch({ type: assetConfigAction.addDeviceFactors, payload }),//新建设备厂家
-  editDeviceFactors: payload => dispatch({ type: assetConfigAction.editDeviceFactors, payload }),//编辑设备厂家
-  deleteDeviceFactors: payload => dispatch({ type: assetConfigAction.deleteDeviceFactors, payload }),//删除设备厂家
-  getDeviceModesList: payload => dispatch({ type: assetConfigAction.getDeviceModesList, payload }),//获取设备型号列表
-  addDeviceModes: payload => dispatch({ type: assetConfigAction.addDeviceModes, payload }),//新建设备型号
-  editDeviceModes: payload => dispatch({ type: assetConfigAction.editDeviceModes, payload }),//编辑设备型号
-  deleteDeviceModes: payload => dispatch({ type: assetConfigAction.deleteDeviceModes, payload }),//删除设备型号
+  getAssetTree: payload => dispatch({ type: assetConfigAction.getAssetTree, payload }), // 台账生产资产树
+  getNodeDetail: payload => dispatch({ type: assetConfigAction.getNodeDetail, payload }), // 台账生产资产树节点详情
+  addAssetNode: payload => dispatch({ type: assetConfigAction.addAssetNode, payload }), //台账增加生产资产节点
+  deleteAssetNode: payload => dispatch({ type: assetConfigAction.deleteAssetNode, payload }), //台账删除生产资产树
+  editAssetNode: payload => dispatch({ type: assetConfigAction.editAssetNode, payload }), //台账编辑生产资产节点
+  getDeviceFactorsList: payload => dispatch({ type: assetConfigAction.getDeviceFactorsList, payload }), //获取设备厂家列表
+  addDeviceFactors: payload => dispatch({ type: assetConfigAction.addDeviceFactors, payload }), //新建设备厂家
+  editDeviceFactors: payload => dispatch({ type: assetConfigAction.editDeviceFactors, payload }), //编辑设备厂家
+  deleteDeviceFactors: payload => dispatch({ type: assetConfigAction.deleteDeviceFactors, payload }), //删除设备厂家
+  getDeviceModesList: payload => dispatch({ type: assetConfigAction.getDeviceModesList, payload }), //获取设备型号列表
+  addDeviceModes: payload => dispatch({ type: assetConfigAction.addDeviceModes, payload }), //新建设备型号
+  editDeviceModes: payload => dispatch({ type: assetConfigAction.editDeviceModes, payload }), //编辑设备型号
+  deleteDeviceModes: payload => dispatch({ type: assetConfigAction.deleteDeviceModes, payload }), //删除设备型号
 
-})
-export default connect(mapStateToProps, mapDispatchToProps)(AssetsConfig)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(AssetsConfig);
