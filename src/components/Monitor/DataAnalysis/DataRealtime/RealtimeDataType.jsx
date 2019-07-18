900a039e4c9ed619ed8235d4e14cda1f3c336497
yanqm <yanqm@cnegroup.com>
2019-07-18 14:28:12 +0800

    修改历史趋势和实时数据bug

--- src/components/Monitor/DataAnalysis/DataRealtime/RealtimeDataType.jsx
+++ src/components/Monitor/DataAnalysis/DataRealtime/RealtimeDataType.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import styles from './realtimeStyle.scss';
 import path from '../../../../constants/path';
-import { message } from 'antd';
+import WarningTip from '../../../Common/WarningTip';
 
 const { APIBasePath } = path.basePaths;
 const { monitor } = path.APISubPaths;
@@ -24,8 +24,15 @@ class RealtimeDataType extends Component {
     stopRealtimeChart: PropTypes.func,
     stopRealtimeList: PropTypes.func,
     endTime: PropTypes.string,
-    startTime: PropTypes.string,
+    startTime: PropTypes.object,
   };
+
+  state = {
+    showModal: false,
+    showDeleteWarning: false,
+    showWarningTip: false,
+    warningTipText: '导出最近半小时内的测点数据'
+  }
 
   showChart = () => { // 若已选测点=>终止当前请求启动图表定时请求,若未选测点则存储属性
     const { changeRealtimeStore, queryParam, getRealtimeChart, stopRealtimeList, chartRealtime } = this.props;
@@ -35,7 +42,7 @@ class RealtimeDataType extends Component {
       getRealtimeChart({ queryParam });
       changeRealtimeStore({
         realtimeType: 'chart',
-        // chartRealtime: {}
+        chartRealtime: {}
       });
     } else {
       changeRealtimeStore({ realtimeType: 'chart' });
@@ -46,7 +53,7 @@ class RealtimeDataType extends Component {
     const { changeRealtimeStore, queryParam, listParam, getRealtimeList, stopRealtimeChart } = this.props;
     const { devicePoints = [] } = queryParam;
     if (devicePoints.length > 0) {
-      stopRealtimeChart(); 
+      // stopRealtimeChart(); // 新需求：在切换图表和列表时，图表之前的数据依旧存在，所以注释了此行代码
       getRealtimeList({ queryParam, listParam });
       changeRealtimeStore({
         realtimeType: 'list',
@@ -57,13 +64,11 @@ class RealtimeDataType extends Component {
     }
   }
 
-  exportRealtime = () => { // '导出实时数据excel'
+  confirmWarningTip = () => {
+    this.setState({ showWarningTip: false })
     const { startTime, downLoadFile, queryParam } = this.props;
     const clickEndTime = moment(); // 点击导出按钮时间
-    if (clickEndTime.diff(startTime, 'minute') > 30) {
-      return message.warning('最多支持导出最近半小时数据');
-    }
-
+  
     const url = `${APIBasePath}${monitor.exportRealtime}`;
     const { deviceFullCodes, devicePoints } = queryParam;
     const timeZone = moment().zone() / (-60);
@@ -81,20 +86,28 @@ class RealtimeDataType extends Component {
       },
     })
   }
+  
+  exportRealtime = () => { // '导出实时数据excel'
+    this.setState({
+      showWarningTip: true,
+    })
+  }
 
   render(){
+    const { warningTipText, showWarningTip } = this.state;
     const { realtimeType, downloading } = this.props;
     return (
       <div className={styles.realtimeDataType}>
+        {showWarningTip && <WarningTip onOK={this.confirmWarningTip} value={warningTipText} />}
         <div className={styles.tabIcons}>
           <Icon onClick={this.showChart} type="bar-chart" className={realtimeType === 'chart'? styles.active : styles.normal} />
           <Icon onClick={this.showList} type="bars" className={realtimeType === 'list'? styles.active : styles.normal} />
         </div>
-        {realtimeType === 'list' && <Button
+        <Button
           className={styles.export}
           onClick={this.exportRealtime}
           loading={downloading}
-        >导出</Button>}
+        >导出</Button>
       </div>
     )
   }
