ffb4cf0d7b7a1fa3a373ef0bdc85669e9e459e9d
zwy414 <1551644698@qq.com>
2019-07-19 17:02:46 +0800

    百分比布局调整

--- src/components/Home/MiniHomepage/MiniComponents/InefficientSeries.jsx
+++ src/components/Home/MiniHomepage/MiniComponents/InefficientSeries.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { dataFormat } from '../../../../utils/utilFunc';
 import styles from './miniComponents.scss';
 
-export const InefficientSeries = ({ inefficientList }) => {
+export const InefficientSeries = ({ inefficientList = [] }) => {
+  const hasInefficient = inefficientList.length > 0 ? true : false;
   return (
     <section className={styles.inefficientSeries}>
       <h3>低效组串预警</h3>
@@ -14,13 +15,16 @@ export const InefficientSeries = ({ inefficientList }) => {
           <span className={styles.lost}>电量损失比(%)</span>
         </div>
         <div className={styles.inefficientList}>
-          {inefficientList.map(e => (
+          {false ? inefficientList.slice(0, 3).map(e => (
             <div key={e.inefficiencyId} className={styles.eachInefficient}>
               <span className={styles.name}>{e.stationName}</span>
               <span className={styles.series}>{e.deviceName}</span>
               <span className={styles.lost}>{dataFormat(e.lostGenPercent)}</span>
             </div>
-          ))}
+          )) : <div className={styles.noData}>
+              <img src="/img/no data_icon.png" />
+              <span className={styles.noneText}>暂无预警</span>
+          </div>}
         </div>
       </div>
     </section>
