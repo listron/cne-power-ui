646c2b1baa2188437ff98d2d3586d3b2b3148748
haojianling <591191280@qq.com>
2019-07-30 16:47:20 +0800

    修改风电站的null

--- src/components/Monitor/StationMonitor/AllStation/NewPvStation/SingleStaionList.jsx
+++ src/components/Monitor/StationMonitor/AllStation/NewPvStation/SingleStaionList.jsx
@@ -1,8 +1,8 @@
 
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './pvStation.scss';
-import { message, Select } from "antd";
+import { message, Select } from 'antd';
 import { Link } from 'react-router-dom';
 import { dataFormats } from '../../../../../utils/utilFunc';
 import { divideFormarts, multiplyFormarts, powerPoint } from '../../PvCommon/PvDataformat';
@@ -27,7 +27,7 @@ class SingleStaionList extends React.Component {
   showTip = (currentStatus) => {
     message.destroy();
     if (currentStatus === '900') {
-      message.config({ top: 225, maxCount: 1, });
+      message.config({ top: 225, maxCount: 1 });
       message.warning('电站未接入,无法查看详情', 2);
     }
   }
@@ -48,17 +48,16 @@ class SingleStaionList extends React.Component {
     const equivalentHours = singleStation.equivalentHours;
     const alarm = singleStation.alarmNum > 0;
     const invertType = singleStation.lowEffType === 1 ? '201' : '206';
-    const arr = ['fadeIn', 'rotateIn', 'zoomIn'];
-    const animate = parseInt(Math.random() * 3);
     return (
-      <div className={`${styles[getStatusName[`${currentStatus}`]]} ${styles.staionCard}  ${alarm && styles.alarm}`} onClick={() => { this.showTip(currentStatus) }} key={singleStation.stationCode} style={{ WebkitAnimation: `${arr[animate]} 0.8s ease-in-out 1 0s alternate forwards` }} >
+      <div className={`${styles[getStatusName[`${currentStatus}`]]} ${styles.staionCard}  ${alarm && styles.alarm}`} onClick={() => { this.showTip(currentStatus); }} key={singleStation.stationCode}>
         <Link to={`/monitor/singleStation/${singleStation.stationCode}`} className={styles.linkBox}>
           <div className={styles.stationTop}>
             <div className={styles.stationName} title={singleStation.stationName}> {singleStation.stationName}</div>
             <div className={styles.staionCapacity}>
               <div>
+                <i className={'iconfont icon-da'} />
                 <span className={styles.changeNum}>
-                  <i className={'iconfont icon-da'}></i> {stationCapacity}</span> {realCapacityUnit}
+                  {dataFormats(stationCapacity, '--', 2)}</span> {realCapacityUnit}
               </div>
               <div className={styles.stationUnitCount}>
                 <span className={styles.changeNum}>{singleStation.stationUnitCount}</span> 台
@@ -111,8 +110,8 @@ class SingleStaionList extends React.Component {
           </Link>
         </div>
       </div>
-    )
+    );
   }
 }
-export default SingleStaionList
+export default SingleStaionList;
 
