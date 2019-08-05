544b3975eed2afc4193fb92616a8f043d3ef1033
Mr-liu <you@example.com>
2019-08-05 16:36:36 +0800

    :sparkles:

---
+++ src/components/HighAnalysis/Achievement/AreaAchieve/AreaChart/AreaChart.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import eCharts from 'echarts';
+import PropTypes from 'prop-types';
+
+import styles from './areaChart.scss';
+
+export default class AreaChart extends Component {
+
+  static propTypes = {
+  };
+
+  componentDidMount() {
+    const { areaChart } = this;
+    const myChart = eCharts.init(areaChart);
+    myChart.setOption(this.drawChart());
+  }
+
+
+  drawChart = () => {
+    return {
+      series: [{
+        type: 'treemap',
+        top: '0%',
+        left: '2%',
+        right: '2%',
+        bottom: '0%',
+        roam: false,
+        breadcrumb: {
+          show: false,
+        },
+        itemStyle: {
+          borderWidth: 1,
+        },
+        data: [{
+          name: 'nodeA', // First tree
+          value: 10,
+          children: [{
+            name: 'nodeAa', // First leaf of first tree
+            value: 4,
+          }, {
+            name: 'nodeAb', // Second leaf of first tree
+            value: 6,
+          }],
+        }, {
+          name: 'nodeB', // Second tree
+          value: 20,
+          children: [{
+            name: 'nodeBa', // Son of first tree
+            value: 20,
+            children: [{
+              name: 'nodeBa1', // Granson of first tree
+              value: 20,
+            }],
+          }],
+        }],
+      }],
+    };
+  };
+
+  render() {
+    return (
+      <div className={styles.areaBox}>
+        <div className={styles.areaBoxTitle}>
+          各电站装机容量
+        </div>
+        <div className={styles.areaChartCenter} ref={ref => {this.areaChart = ref;}} />
+      </div>
+    );
+  }
+}
---
+++ src/components/HighAnalysis/Achievement/AreaAchieve/AreaTrendChart/AreaTrendChart.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import eCharts from 'echarts';
+import { Radio } from 'antd';
+import PropTypes from 'prop-types';
+
+import styles from './areaTrendChart.scss';
+
+export default class AreaTrendChart extends Component {
+
+  static propTypes = {
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: '2',
+    };
+  }
+
+  componentDidMount() {
+    const { trendChart } = this;
+    const myChart = eCharts.init(trendChart);
+    myChart.setOption(this.drawChart());
+  }
+
+  drawChart = () => {
+    return {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow', // 默认为直线，可选为：'line' | 'shadow'
+        },
+      },
+      grid: {
+        top: '10%',
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true,
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          name: 'PBA',
+        },
+      ],
+      dataZoom: [{
+        start: 0,
+        end: 100,
+        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+        handleSize: '80%',
+        handleStyle: {
+          color: '#fff',
+          shadowBlur: 3,
+          shadowColor: 'rgba(0, 0, 0, 0.6)',
+          shadowOffsetX: 2,
+          shadowOffsetY: 2,
+        },
+      }],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '10',
+          itemStyle: {
+            barBorderRadius: [5, 5, 0, 0],
+          },
+          data: [10, 52, 200, 334, 390, 330, 220],
+        },
+      ],
+    };
+  };
+
+  render() {
+    const { status } = this.state;
+    return (
+      <div className={styles.areaTrendBox}>
+        <div className={styles.areaTrendTitle}>
+          <span>PBA趋势图</span>
+          <Radio.Group value={status} buttonStyle="solid" onChange={this.handleStatusChange}>
+            <Radio.Button value="1">按日</Radio.Button>
+            <Radio.Button value="2">按月</Radio.Button>
+            <Radio.Button value="3">按年</Radio.Button>
+          </Radio.Group>
+        </div>
+        <div className={styles.trendCenter} ref={ref => {this.trendChart = ref;}} />
+      </div>
+    );
+  }
+}
