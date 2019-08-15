3b5298d9136d0cfdb5d4c6c04b2ee7b676920970
haojianling <591191280@qq.com>
2019-08-15 14:50:09 +0800

    修改图标位置

--- src/components/Layout/MenuBoard.jsx
+++ src/components/Layout/MenuBoard.jsx
@@ -80,9 +80,7 @@ class MenuBoard extends Component {
     const { linkInfo } = this.state;
     const { menuBoardShow, menuBoardRequired } = this.props;
     const enterpriseId = Cookie.get('enterpriseId');
-    // const needBoardIcon = menuBoardRequired.includes(enterpriseId);
-    const needBoardIcon =true;
-
+    const needBoardIcon = menuBoardRequired.includes(enterpriseId);
     return (
       <div className={styles.menuBoard}>
         {needBoardIcon && <div className={styles.boardIcon} onClick={this.toggleBoard}>
