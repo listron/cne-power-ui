54850ecc34275f452f0261dfe9eeb63dccab7470
zwy414 <1551644698@qq.com>
2019-08-29 14:53:06 +0800

    通用选择组件维护

--- src/components/Common/AutoSelect/AutoModal.jsx
+++ src/components/Common/AutoSelect/AutoModal.jsx
@@ -47,7 +47,7 @@ class AutoModal extends Component {
   handleOK = () => {
     const { infoLists, onValueCheck } = this.props;
     const { checkedTrees } = this.state;
-    const valueResult = infoLists.filter(e => checkedTrees.includes(`${e.value}`));
+    const valueResult = infoLists.filter(e => checkedTrees.some(treeNode => `${e.value}` === `${treeNode}`));
     this.setState({ isShow: false });
     onValueCheck(valueResult);
   }
