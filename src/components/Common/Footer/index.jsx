56b93a99dfcfac539767e5ece7ee1f15ae63e5e6
武静 <wuaini2006@msn.com>
2019-09-03 21:16:28 +0800

    还原版本

--- src/components/Common/Footer/index.jsx
+++ src/components/Common/Footer/index.jsx
@@ -17,7 +17,7 @@ class Footer extends Component {
     const theme = Cookie.get('theme') || 'light';
     return (
       <div className={`${styles.footer} ${theme === 'dark' ? styles.darkFooter : styles.lightFooter}`}>
-        {/* <span className={styles.footerText}>京ICP备12030847号-2 © 2017-2019 北京动力协合科技有限公司</span> */}
+        <span className={styles.footerText}>京ICP备12030847号-2 © 2017-2019 北京动力协合科技有限公司</span>
       </div>
     );
   }
