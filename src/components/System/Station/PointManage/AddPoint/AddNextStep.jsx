e72bd28af2a11c9111403211cf9dcb851fdaa82e
tiandali <tian.chengli@outlook.com>
2019-09-06 11:17:16 +0800

    新增

--- src/components/System/Station/PointManage/AddPoint/AddNextStep.jsx
+++ src/components/System/Station/PointManage/AddPoint/AddNextStep.jsx
@@ -34,13 +34,107 @@ class AddNextStep extends React.Component {
               )}
             </FormItem>
             <FormItem label="测点描述" colon={false} className={styles.formItemStyle}>
-              {getFieldDecorator('manufacturer', {
+              {getFieldDecorator('devicePointName', {
                 initialValue: null,
                 rules: [{ required: true, message: '请正确填写', type: 'string', max: 30 }],
               })(
                 <Input placeholder="请输入" />
               )}
             </FormItem>
+            <FormItem label="第三方测点名称" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('devicePointCode', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+            <FormItem label="英文名称" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('devicePointIecname', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="数据类型" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('devicePointDatatype', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="测点类型" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('devicePointType', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="单位" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('devicePointUnit', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="系数" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('devicePointIndex', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="小数点位数" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('devicePointDecimalplace', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="是否上传" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('manufacturer', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="是否显示" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('manufacturer', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+            <FormItem label="理论最大值" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('maxTheory', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+            <FormItem label="理论最小值" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('minTheory', {
+                initialValue: null,
+              })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+            <FormItem label="偏差值" colon={false} className={styles.formItemStyle}>
+              {getFieldDecorator('discrepancy', {}
+                initialValue: null,
+            })(
+                <Input placeholder="请输入" />
+              )}
+            </FormItem>
+
+
 
           </Form>
         </div>
