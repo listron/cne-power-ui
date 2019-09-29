2cde7f920aca45dff828049bacd42af1b67743d2
zwy414 <1551644698@qq.com>
2019-09-29 14:14:43 +0800

    环境变量预配置

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,19 +1,34 @@
 import $ from 'jquery';
 // import apiConfig from './api_config.json'
 const randomNum = Math.random();
+// console.log('my environment test',ENV, DEV, PRO);
 const jsonText = $.ajax({
-	type: "get",
-	dataType: "json",
+	type: 'get',
+	dataType: 'json',
 	url: `/powerp.json?${randomNum}`,
 	async: false,
 }).responseText;
 
 const apiConfig = $.parseJSON(jsonText);
 
+// cross-env NODE_ENV=development
+// const apiUrlReal = {
+// 	DEVELOP: 'http://10.10.15.51',
+// 	TEST: 'http://10.10.15.83',
+// 	AWS: '',
+// 	PRODUCTION: '',
+// };
+// module.exports = {
+// 	originUri: apiUrlReal,
+//   apiHostUri: apiUrlReal + '/api',
+// 	tokenUri: apiUrlReal + '/token',
+// 	apiUrlReal: apiUrlReal,
+// };
+
+
 // 搭配环境调整环境只需要在powerp.json中找到public内environment对应字符串匹配develop，demo，test或official即可,
 // 同时为了保证cookie登录权限：powerp.json中将domain由chinacloudsites.cn改为localhost
-const environmentUrl = apiConfig.public.environment
-
+const environmentUrl = apiConfig.public.environment;
 module.exports = {
 	originUri: apiConfig[environmentUrl].apiUrlReal,
   apiHostUri: apiConfig[environmentUrl].apiUrlReal + '/api',
@@ -21,4 +36,4 @@ module.exports = {
 	outUrl: '',
 	apiUrlReal: apiConfig[environmentUrl].apiUrlReal,
 	environment: environmentUrl,
-}
+};
