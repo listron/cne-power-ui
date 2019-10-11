b7727ef49d941b4cc3b0ff3979b9ab520762274f
zwy414 <1551644698@qq.com>
2019-10-11 16:34:17 +0800

    单位添加

--- src/components/Monitor/DataAnalysis/Overview/PointsOverview/PointsList.jsx
+++ src/components/Monitor/DataAnalysis/Overview/PointsOverview/PointsList.jsx
@@ -12,10 +12,12 @@ class PointsList extends PureComponent{
     theme: PropTypes.string,
     pointsLoading: PropTypes.bool,
     pointsData: PropTypes.array,
+    pointList: PropTypes.array,
   }
 // histogram: "[[0.0, 15.0, 0.1034], [15.0, 30.0, 0.1034], [30.0, 45.0, 0.1034], [45.0, 60.0, 0.0], [60.0, 75.0, 0.0517], [75.0, 90.0, 0.1034], [90.0, 105.0, 0.1552], [105.0, 120.0, 0.069], [120.0, 135.0, 0.0345], [135.0, 150.0, 0.0], [150.0, 165.0, 0.0], [165.0, 180.0, 0.0], [180.0, 195.0, 0.0172], [195.0, 210.0, 0.0172], [210.0, 225.0, 0.2414]]"
   render(){
-    const { pointsData = [], pointsLoading, theme } = this.props;
+    const { pointsData = [], pointsLoading, theme, pointList } = this.props;
+    // console.log(pointList);
     return(
       <div className={styles.pointsList}>
         {pointsData.map(e => {
@@ -26,9 +28,10 @@ class PointsList extends PureComponent{
           } catch(err){
             console.log(err);
           }
+          const { unit } = pointList.find(m => m.value === e.pointCode) || {};
           return (
             <div key={e.pointCode} className={styles.pointColumn}>
-              <BaseInfo {...e} />
+              <BaseInfo {...e} unit={unit} />
               <PointChart histogramList={histogramList} pointsLoading={pointsLoading} theme={theme} />
               <ValueInfo {...e} />
             </div>
