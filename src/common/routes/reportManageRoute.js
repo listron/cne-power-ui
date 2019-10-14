3d2c3d15d1f349a8c8a5a3f149673a17424296e7
tiandali <tian.chengli@outlook.com>
2019-10-14 10:18:37 +0800

    屏蔽报表管理

--- src/common/routes/reportManageRoute.js
+++ src/common/routes/reportManageRoute.js
@@ -4,7 +4,8 @@ import { lazy } from 'react';
 export const reportManageRoute = [
   {
     path: '/report/station',
-    component: lazy(() => import('../../containers/ReportManage/ReportStation/ReportStation.jsx')),
+    // component: lazy(() => import('../../containers/ReportManage/ReportStation/ReportStation.jsx')),
+    component: lazy(() => import('../../components/Common/Building/Building.jsx')),
   },
 
 ];
