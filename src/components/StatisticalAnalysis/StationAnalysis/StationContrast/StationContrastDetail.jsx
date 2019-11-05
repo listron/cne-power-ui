838764dccefe51423f5d796f02e07ab8a60c3f98
tiandali <tian.chengli@outlook.com>
2019-11-05 21:28:35 +0800

    电站对比的样式，以及电站报表table的样式

--- src/components/StatisticalAnalysis/StationAnalysis/StationContrast/StationContrastDetail.jsx
+++ src/components/StatisticalAnalysis/StationAnalysis/StationContrast/StationContrastDetail.jsx
@@ -9,6 +9,7 @@ import { stationContrastBaseInfo, stationContrastDataInfo } from '../../../../co
 import { Gradient1, Gradient2, barRadius, chartsLoading, themeConfig, chartsNodata } from '../../../../utils/darkConfig';
 import { dataFormats } from '../../../../utils/utilFunc';
 
+
 class StationContrastDetail extends Component {
   static propTypes = {
     stationContrastDetail: PropTypes.array,
@@ -34,7 +35,6 @@ class StationContrastDetail extends Component {
       this.drawChart(stationContrastDetail, column, theme);
     }
   }
-
   getColor = {
     'light': ['#199475', '#c7ceb2'],
     'dark': [Gradient1, Gradient2],
@@ -128,9 +128,6 @@ class StationContrastDetail extends Component {
     };
     stationContrastDiagram.setOption(stationContrastOption);
   }
-
-
-
   render() {
     return (
       <div ref={(ref) => { this.charts = ref; }} className={styles.stationContrastDetailBox} />
