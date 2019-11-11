1a2916963af269b97798ae382679cb027be5d1a7
haojianling <591191280@qq.com>
2019-11-11 17:12:48 +0800

    暂存

--- src/components/Operation/WorkProcess/Defect/DefectSearch.jsx
+++ src/components/Operation/WorkProcess/Defect/DefectSearch.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './defect.scss';
 import FilterConditions from '@components/Common/FilterConditions/FilterCondition';
 import Search from 'antd/lib/input/Search';
+import FilterCondition from '@components/Common/FilterConditions/FilterCondition';
 
 class DefectSearch extends Component {
   static propTypes = {
@@ -19,10 +20,63 @@ class DefectSearch extends Component {
   }
 
   render() {
-    const { theme } = this.props;
+    const { theme, stations } = this.props;
     return (
       <div className={`${styles.seacrchCont} ${styles[theme]}`}>
-        查询条件
+        <FilterConditions
+          onChange={this.filterConditionChange}
+          theme={theme}
+          option={[
+            {
+              name: ' 发生时间',
+              type: 'time',
+              typeName: 'rangeTimes',
+            },
+            {
+              name: '电站类型',
+              type: 'stationType',
+              typeName: 'stationType',
+            },
+            {
+              name: '电站名称',
+              type: 'parentCheckBox',
+              typeName: 'stationCodes',
+              rules: ['stationName', 'stationCode'],
+              parentName: 'provinceName',
+              data: stations,
+            },
+            {
+              name: '设备类型',
+              type: 'multipleType',
+              typeName: 'deviceTypeCode',
+              rules: ['deviceTypeName', 'deviceTypeCode'],
+              data: deviceTypes,
+            },
+            {
+              name: '缺陷级别',
+              type: 'defectLevel',
+              typeName: 'defectLevel',
+            },
+            {
+              name: '缺陷类型',
+              type: 'parentCheckBox',
+              typeName: 'defectTypeCode',
+              parentName: 'parentName',
+              rules: ['name', 'id'],
+              data: defectTypeList,
+            },
+            {
+              type: 'defectSource',
+              typeName: 'defectSource',
+            },
+            {
+              name: '我参与的',
+              type: 'switch',
+              typeName: 'join',
+            },
+          ]}
+          value={{ stationType, stationCodes, defectSource, defectLevel, deviceTypeCode, defectTypeCode, rangeTimes: [createTimeStart, createTimeEnd], join: handleUser }}
+        />
       </div>
     );
   }
