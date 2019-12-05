7d291b1039cdcf165eeb683f40c75f9adf43f02a
zwy414 <1551644698@qq.com>
2019-12-05 11:24:26 +0800

    eslint规范

--- src/components/System/Account/PersonnelManage/PersonnelMain/PersonnelList/List.jsx
+++ src/components/System/Account/PersonnelManage/PersonnelMain/PersonnelList/List.jsx
@@ -55,7 +55,7 @@ class List extends Component {
           return <span className={statusText === '待审核' ? styles.toExamine : styles.status}>{statusText || '--'}</span>;
         },
         sorter: true,
-      }
+      },
     ],
   }
 
