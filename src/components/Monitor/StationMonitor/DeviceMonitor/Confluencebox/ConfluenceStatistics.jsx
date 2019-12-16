378441ee4e8f365d24edbe9b6eebc2f89b1a864b
yanqm <yanqm@cnegroup.com>
2019-12-16 14:54:29 +0800

    修改电站监控异常数据颜色

--- src/components/Monitor/StationMonitor/DeviceMonitor/Confluencebox/ConfluenceStatistics.jsx
+++ src/components/Monitor/StationMonitor/DeviceMonitor/Confluencebox/ConfluenceStatistics.jsx
@@ -10,7 +10,7 @@ import styles from './confluencebox.scss';
 const EachRecord = ({ text, value, unit, show }) => (
   <div className={styles.eachRecord}>
     <span className={styles.text}>{text}</span>
-    <span className={`${styles.specialColor} ${show && styles.specialColor}`}><DeviceValueFormat value={value} /></span>
+    <span className={show && styles.specialColor}><DeviceValueFormat value={value} /></span>
     <span className={styles.unit}>{unit}</span>
     {show &&
     <span className={styles.tooltipName}>
