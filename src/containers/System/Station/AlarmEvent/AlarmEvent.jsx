a859e249c8b9badaff5182127921893e21b5f238
haojianling <591191280@qq.com>
2020-01-10 15:56:12 +0800

    去掉使用的新插件

--- src/containers/System/Station/AlarmEvent/AlarmEvent.jsx
+++ src/containers/System/Station/AlarmEvent/AlarmEvent.jsx
@@ -10,32 +10,8 @@ import Footer from '../../../../components/Common/Footer';
 import PropTypes from 'prop-types';
 import Cookie from 'js-cookie';
 
-const mapStateToProps = (state) => {
-  return ({
-    ...state.system.alarmEventReducer.toJS(),
-    stations: state.common.get('stations').toJS(),
-  });
-};
 
-const mapDispatchToProps = (dispatch) => ({
-  resetStore: payload => dispatch({ type: alarmEventAction.resetStore, payload }),
-  changeStore: payload => dispatch({ type: alarmEventAction.changeStore, payload }),
-  getDiagVersion: payload => dispatch({ type: alarmEventAction.getDiagVersion, payload }),
-  editVersion: payload => dispatch({ type: alarmEventAction.editVersion, payload }),
-  updateVersion: payload => dispatch({ type: alarmEventAction.updateVersion, payload }),
-  delVersion: payload => dispatch({ type: alarmEventAction.delVersion, payload }),
-  getVersionEvent: payload => dispatch({ type: alarmEventAction.getVersionEvent, payload }),
-  getEditVersionStation: payload => dispatch({ type: alarmEventAction.getEditVersionStation, payload }),
-  addVersionEvent: payload => dispatch({ type: alarmEventAction.addVersionEvent, payload }),
-  editVersionEvent: payload => dispatch({ type: alarmEventAction.editVersionEvent, payload }),
-  delVersionEvent: payload => dispatch({ type: alarmEventAction.delVersionEvent, payload }),
-  getAlarmEvent: payload => dispatch({ type: alarmEventAction.getAlarmEvent, payload }),
-  getPointList: payload => dispatch({ type: alarmEventAction.getPointList, payload }),
-  getVersionStation: payload => dispatch({ type: alarmEventAction.getVersionStation, payload }),
-});
-
-@connect(mapStateToProps, mapDispatchToProps)
-export default class AlarmEvent extends Component {
+class AlarmEvent extends Component {
   static propTypes = {
     resetStore: PropTypes.func,
     getDiagVersion: PropTypes.func,
@@ -74,3 +50,29 @@ export default class AlarmEvent extends Component {
   }
 }
 
+
+const mapStateToProps = (state) => {
+  return ({
+    ...state.system.alarmEventReducer.toJS(),
+    stations: state.common.get('stations').toJS(),
+  });
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  resetStore: payload => dispatch({ type: alarmEventAction.resetStore, payload }),
+  changeStore: payload => dispatch({ type: alarmEventAction.changeStore, payload }),
+  getDiagVersion: payload => dispatch({ type: alarmEventAction.getDiagVersion, payload }),
+  editVersion: payload => dispatch({ type: alarmEventAction.editVersion, payload }),
+  updateVersion: payload => dispatch({ type: alarmEventAction.updateVersion, payload }),
+  delVersion: payload => dispatch({ type: alarmEventAction.delVersion, payload }),
+  getVersionEvent: payload => dispatch({ type: alarmEventAction.getVersionEvent, payload }),
+  getEditVersionStation: payload => dispatch({ type: alarmEventAction.getEditVersionStation, payload }),
+  addVersionEvent: payload => dispatch({ type: alarmEventAction.addVersionEvent, payload }),
+  editVersionEvent: payload => dispatch({ type: alarmEventAction.editVersionEvent, payload }),
+  delVersionEvent: payload => dispatch({ type: alarmEventAction.delVersionEvent, payload }),
+  getAlarmEvent: payload => dispatch({ type: alarmEventAction.getAlarmEvent, payload }),
+  getPointList: payload => dispatch({ type: alarmEventAction.getPointList, payload }),
+  getVersionStation: payload => dispatch({ type: alarmEventAction.getVersionStation, payload }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlarmEvent);
