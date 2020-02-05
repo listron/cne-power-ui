cf08c555e15c779724226f6982cfc34077ac6945
zwy <1551644698@qq.com>
2020-02-05 18:48:18 +0800

    表格排序交互

--- src/components/Common/Power/CneTable/index.jsx
+++ src/components/Common/Power/CneTable/index.jsx
@@ -4,10 +4,31 @@ import { Table } from 'antd';
 import PropTypes from 'prop-types';
 import styles from './index.scss';
 
+/**
+ * 现阶段要求所有默认表格只有升序, 降序两种模式,
+ * 提前保留props:isSortMode: bool默认false, true时排序模式(升序, 降序, 无序)
+ * sortField手动指定得排序字段:string, 与dataIndex对应
+ * sortMethod手动指定排序方式:string: ascend descend
+ */
+
 export default function CneTable ({ ...props }) {
-  const { theme = 'light', className, ...rest } = props;
+  const { theme = 'light', className, sortField, sortMethod, columns, ...rest } = props;
+  let tableColumn = [...columns];
+  if (sortField && sortMethod) {
+    tableColumn = columns.map(e => {
+      const { sorter, dataIndex } = e || {};
+      if (sorter && sortField === dataIndex) {
+        return {
+          ...e,
+          sortOrder: sortMethod,
+        };
+      }
+      return { ...e };
+    });
+  }
   return (
     <Table
+      columns={tableColumn}
       className={`${styles.cneTable} ${className} ${styles[theme]}`}
       locale={{ emptyText: <img width="223" height="164" src="/img/nodata.png" /> }}
       pagination={false}
@@ -18,27 +39,8 @@ export default function CneTable ({ ...props }) {
 
 CneTable.propTypes = {
   theme: PropTypes.string,
+  sortField: PropTypes.string,
+  sortMethod: PropTypes.string,
   className: PropTypes.string,
+  columns: PropTypes.array,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
