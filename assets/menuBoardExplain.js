29eff963d25265d35298d5f3309f7d586470711d
zwy <1551644698@qq.com>
2020-02-26 17:35:59 +0800

    配置参数添加
    (cherry picked from commit ac6635b130eebd683f27f2d03b207189db9f6dbd)

---
+++ assets/menuBoardExplain.js
@@ -0,0 +1,13 @@
+
+
+// menuBoardRequired.json
+// 部分企业配置项
+{
+  // "menuBoardRequired": ["316696409677824"], 登录成功后是否展示快捷提示链接页
+  // "miniHomepageIds": ["316696409677824"], 缩小另类主题主页
+  // "fullScreenHomepageIds": ["316696409677824"], 缩小一屏版主页
+  // "screenAddress": "https://screen.cnecloud.com", 大屏链接地址
+  // "reportAddress": "https://report.epower.com", 报表地址
+}
+
+// powerp.json各个运行环境api, 及public各环境基础信息配置
