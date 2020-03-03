4a2ef96c3f42abac5b84e87311ad681e0db000b0
zwy <1551644698@qq.com>
2020-03-03 18:28:42 +0800

    暂存代码块

---
+++ src/components/Operation/NewWorkProcess/TestEliminateDefects/ListPage/EliminateDefectsHandler.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import CneButton from '@components/Common/Power/CneButton';
+import CommonPagination from '@components/Common/CommonPagination';
+import { Radio } from 'antd';
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+import styles from './listPage.scss';
+
+export default class EliminateDefectsHandler extends Component {
+
+  addDefect = () => {
+    console.log('新建消缺工单');
+  }
+
+  onChangeStatus = (value) => {
+    console.log(value);
+  }
+
+  onPaginationChange = ({ currentPage, pageSize }) => {
+    console.log(currentPage, pageSize);
+  }
+
+  render() {
+    const defectStatic = {
+      toConfirm: 12,
+      toReceive: 22,
+      toDo: 221,
+      toExamine: 21,
+    };
+    return (
+      <div className={styles.eliminateDefectsHandler}>
+        <div>
+          <CneButton className={styles.addBtn} onClick={this.addDefect}>
+            <i className={`iconfont icon-newbuilt ${styles.addIcon}`} />
+            <span className={styles.text}>新建</span>
+          </CneButton>
+          <RadioGroup onChange={this.onChangeStatus} value="0">
+            <RadioButton value="all">全部</RadioButton>
+            <RadioButton value="0">{`待确认  ${defectStatic.toConfirm}`}</RadioButton>
+            <RadioButton value="1">{`待领取  ${defectStatic.toReceive}`}</RadioButton>
+            <RadioButton value="2">{`执行中  ${defectStatic.toDo}`}</RadioButton>
+            <RadioButton value="3">{`待验收  ${defectStatic.toExamine}`}</RadioButton>
+            <RadioButton value="4">已结单</RadioButton>
+          </RadioGroup>
+        </div>
+        <CommonPagination
+          pageSize={30}
+          currentPage={1}
+          total={800}
+          onPaginationChange={this.onPaginationChange}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Operation/NewWorkProcess/TestEliminateDefects/ListPage/EliminateDefectsList.jsx
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import CneTable from '@components/Common/Power/CneTable';
+import styles from './listPage.scss';
+
+export default class eliminateDefectsList extends Component {
+
+  defectColumn = [
+    {
+      title: '电站名称',
+      dataIndex: 'stationName',
+      sorter: true,
+      className: styles.stationName,
+      render: (text) => (
+        <div
+          className={styles.stationNameText}
+          title={text || ''}
+        >{text || '--'}</div>
+      ),
+    }, {
+      title: '工单描述',
+      dataIndex: 'defectDesc',
+      className: styles.defectDesc,
+      render: (text) => (
+        <div
+          className={styles.defectDescText}
+          title={text || ''}
+        >{text || '--'}</div>
+      ),
+    }, {
+      title: '事件数量',
+      dataIndex: 'eventNum',
+      className: styles.eventNum,
+      sorter: true,
+      render: (text) => (
+        <div
+          className={styles.eventNumText}
+          title={text || ''}
+        >{text || '--'}</div>
+      ),
+    }, {
+      title: '工单创建时间',
+      dataIndex: 'createTime',
+      className: styles.createTime,
+      sorter: true,
+      render: (text) => (
+        <div
+          className={styles.createTimeText}
+          title={text || ''}
+        >{text || '--'}</div>
+      ),
+    }, {
+      title: '执行人',
+      dataIndex: 'handlers',
+      className: styles.handlers,
+      sorter: true,
+      render: (text) => (
+        <div
+          className={styles.handlersText}
+          title={text || ''}
+        >{text || '--'}</div>
+      ),
+    }, {
+      title: '状态',
+      dataIndex: 'status',
+      className: styles.status,
+      sorter: true,
+      render: (text) => (
+        <div
+          className={styles.statusText}
+          title={text || ''}
+        >{text || '--'}</div>
+      ),
+    }, {
+      title: '操作',
+      dataIndex: 'handle',
+      className: styles.handle,
+      sorter: true,
+      render: (text, record) => (
+        <div
+          className={styles.handleText}
+        >
+          <span title="查看" onClick={() => this.onDetailSearch(record)}>查看</span>
+        </div>
+      ),
+    },
+  ]
+
+  onDetailSearch =(record) => {
+    console.log(record);
+  }
+
+  tableSortChange = (pagination, filter, sorter) => {
+    const { field } = sorter || {};
+    console.log(field);
+    // 排序字段
+    // const { sortField, sortMethod } = listParams || {};
+    // let newField = sortField, newSort = 'desc';
+    // if(!field || sortField === sortFieldMap[field]) {// 点击的是正在排序的列
+      // newSort = sortMethod === 'desc' ? 'asc' : 'desc'; // 交换排序方式
+    // }else{
+      // newField = sortFieldMap[field];
+    // }
+  }
+
+  render() {
+    const defectListData = [
+      {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      }, {
+        stationName: '电站1',
+        defectDesc: '这是随手写的静态数据',
+        eventNum: 12,
+        createTime: '21天22小时08分',
+        handlers: '张三, 李四, 王五, 赵六, 祝七, 黑八, 林九',
+        status: '已结单',
+      },
+    ];
+    const sortField = 'stationName';
+    const sortMethod = 'descend';
+    return (
+      <div className={styles.eliminateDefectsList}>
+        <CneTable
+          sortField={sortField}
+          sortMethod={sortMethod}
+          onChange={this.tableSortChange}
+          columns={this.defectColumn}
+          // className={styles.tableStyles}
+          dataSource={defectListData}
+        />
+      </div>
+    );
+  }
+}
