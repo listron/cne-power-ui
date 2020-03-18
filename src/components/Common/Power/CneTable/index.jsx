034d63bd464542d9b2a7f0bcbf775ed76749f03c
zwy <1551644698@qq.com>
2020-03-18 17:37:05 +0800

    缺陷分支

--- src/components/Common/Power/CneTable/index.jsx
+++ src/components/Common/Power/CneTable/index.jsx
@@ -9,31 +9,37 @@ import styles from './index.scss';
  * sortField手动指定得排序字段:string, 与dataIndex对应
  * sortMethod手动指定排序方式:string: ascend descend
  * dataError 数据错误时, 占位图片为数据错误; 无数据时，占位图片为无数据;
+ * column内参数textAlign: left /middle / right / none;
  */
 
 export default function CneTable ({ ...props }) {
   const {
     theme = 'light',
-    className, sortField, sortMethod, columns, dataError,
+    className, sortField, sortMethod, columns, dataError, noMoreDataPic,
     ...rest
   } = props;
   let tableColumn = [...columns];
   if (sortField && sortMethod) {
     tableColumn = columns.map(e => {
-      const { sorter, dataIndex } = e || {};
+      const { sorter, dataIndex, textAlign, className } = e || {};
       if (sorter && sortField === dataIndex) {
-        return {
-          ...e,
-          sortOrder: sortMethod,
-        };
+        e.sortOrder = sortMethod;
+      }
+      // textAlign: 默认=left(左padding10px 居左), middle(无padding居中), right(右padding10px居右), none(无padding且居左)
+      if (textAlign) {
+        e.className = className ? `${className} ${textAlign}Content` : `${textAlign}Content`;
       }
       return { ...e };
     });
   }
+  let totalClassName = `${styles.cneTable} ${className || ''} ${styles[theme] || ''}`;
+  if (noMoreDataPic) { // 需展示 - 没有更多数据 图片
+    totalClassName = `${totalClassName} ${styles.nomore}`;
+  }
   return (
     <Table
       columns={tableColumn}
-      className={`${styles.cneTable} ${className} ${styles[theme]}`}
+      className={totalClassName}
       locale={{
         emptyText: dataError ? <img
           width="84" height="77" src="/img/datawrong.png"
@@ -48,6 +54,7 @@ export default function CneTable ({ ...props }) {
 
 CneTable.propTypes = {
   theme: PropTypes.string,
+  noMoreDataPic: PropTypes.bool,
   dataError: PropTypes.bool,
   sortField: PropTypes.string,
   sortMethod: PropTypes.string,
