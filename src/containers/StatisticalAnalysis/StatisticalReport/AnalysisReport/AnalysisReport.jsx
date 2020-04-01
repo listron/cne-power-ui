aca19272d6db11793c68b34e24f91df361e44718
Mr-liu <you@example.com>
2020-04-01 16:39:31 +0800

    :sprakles: :lipstick: 集团月报

--- src/containers/StatisticalAnalysis/StatisticalReport/AnalysisReport/AnalysisReport.jsx
+++ src/containers/StatisticalAnalysis/StatisticalReport/AnalysisReport/AnalysisReport.jsx
@@ -9,6 +9,7 @@ import Cookie from 'js-cookie';
 import PropTypes from 'prop-types';
 
 import styles from './analysisReport.scss';
+const { MonthPicker } = DatePicker;
 
 const { APIBasePath } = path.basePaths;
 const { getReportDay } = path.APISubPaths.statisticalAnalysis;
@@ -20,11 +21,19 @@ class AnalysisReport extends Component {
 
   constructor(props) {
     super(props);
+    // 每个月1号的凌晨3点
+    const monthStartDate = moment().startOf('month').format('YYYY-MM-DD 03:00:00');
+    // 每个月的当前时间
+    const monthNowDate = moment().format('YYYY-MM-DD HH:mm:ss');
+    // 每个月当前时间和当月1号时间差
+    const diffSeconds = moment(monthStartDate).diff(moment(monthNowDate), 'seconds');
     this.state = {
       // 比较当前时间，小时数是否大于3，也就是凌晨三点
       // 凌晨三点前，默认显示前天，可以选择前天以前的时间；凌晨三点后，默认显示昨天自然天，可以选择昨天以前的时间。
       dayDate: Number(moment().format('HH')) >= 3 ? moment().subtract(1, 'day') : moment().subtract(2, 'day'),
-      typeDowning: '', // dayReport
+      // 每个月1号凌晨三点前，默认显示上上月，可以选择上上月以前的时间；凌晨三点后，默认显示上月，可以选择上月以前的时间。
+      monthDate: diffSeconds >= 0 ? moment().subtract(2, 'months') : moment().subtract(1, 'months'),
+      typeDowning: '', // dayReport monthReport
     };
     this.authData = localStorage.getItem('authData');
   }
@@ -35,9 +44,26 @@ class AnalysisReport extends Component {
     return Number(moment().format('HH')) >= 3 ? current && current > moment().subtract(1, 'days') : current && current > moment().subtract(2, 'days');
   };
 
-  ChangeReportDate = value => { // 日报
+  disabledMonthDate = (current) => { // 月份不可选
+    // 每个月1号的凌晨3点
+    const monthStartDate = moment().startOf('month').format('YYYY-MM-DD 03:00:00');
+    // 每个月的当前时间
+    const monthNowDate = moment().format('YYYY-MM-DD HH:mm:ss');
+    // 每个月当前时间和当月1号时间差
+    const diffSeconds = moment(monthStartDate).diff(moment(monthNowDate), 'seconds');
+    // 比较每月1号凌晨三点前，默认显示上上月，可以选择上上月以前的时间；凌晨三点后，默认显示上月，可以选择上上月以前的时间。
+    return diffSeconds >= 0 ? current && current > moment().subtract(2, 'months') : current && current > moment().subtract(1, 'months');
+  };
+
+  changeReportDate = value => { // 日报
     this.setState({
       dayDate: value,
+    });
+  };
+
+  changeMonthDate = value => { // 月报
+    this.setState({
+      monthDate: value,
     });
   };
 
@@ -52,6 +78,12 @@ class AnalysisReport extends Component {
     if (typeDowning !== 'dayReport') {
       this.downLoadFun(downloadHref, fileName, newDate);
     }
+  };
+
+  downloadMonthReport = () => { // 月报下载
+    const { typeDowning } = this.state;
+    console.log(typeDowning, 'typeDowning');
+    this.setState({ typeDowning: 'monthReport' });
   };
 
   downLoadFun = (url, fileName, date) => { // 根据路径，名称，日期，通用下载函数。
@@ -82,7 +114,7 @@ class AnalysisReport extends Component {
   };
 
   render() {
-    const { dayDate, typeDowning } = this.state;
+    const { dayDate, typeDowning, monthDate } = this.state;
     const { theme } = this.props;
     return (
       <div className={`${styles.analysisReportBox} ${styles[theme]}`}>
@@ -100,7 +132,7 @@ class AnalysisReport extends Component {
                 <DatePicker
                   disabledDate={this.disabledDate}
                   placeholder={'选择时间'}
-                  onChange={this.ChangeReportDate}
+                  onChange={this.changeReportDate}
                   value={dayDate}
                   getCalendarContainer={() => this.refs.wrap}
                 />
@@ -111,6 +143,33 @@ class AnalysisReport extends Component {
                   onClick={this.downloadReport}
                   disabled={!dayDate}
                   loading={typeDowning === 'dayReport'}
+                >
+                  下载
+                </Button>
+              </div>
+            </div>
+            <div className={styles.dailyBox}>
+              <div className={styles.boxTop}>
+                <div className={styles.dayReport}>
+                  <Icon type="download" style={{ color: '#ffffff' }} />
+                </div>
+                <span className={styles.title}>集团月报</span>
+              </div>
+              <div className={styles.dateSearch}>
+                <MonthPicker
+                  disabledDate={this.disabledMonthDate}
+                  placeholder={'选择时间'}
+                  onChange={this.changeMonthDate}
+                  value={monthDate}
+                  getCalendarContainer={() => this.refs.wrap}
+                />
+              </div>
+              <div className={styles.downloadBtn}>
+                <Button
+                  className={styles.text}
+                  onClick={this.downloadMonthReport}
+                  disabled={!dayDate}
+                  loading={typeDowning === 'monthReport'}
                 >
                   下载
                 </Button>
