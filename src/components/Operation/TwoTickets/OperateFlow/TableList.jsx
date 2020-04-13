826ac5c396b67b0b0167f17b5a9bb69de4da53e7
yanqm <yanqm@cnegroup.com>
2020-04-13 17:49:15 +0800

    优化两票列表按钮颜色
    (cherry picked from commit 50b4ab3eaad6b87831a234c2f8a42215113b4e1e)

--- src/components/Operation/TwoTickets/OperateFlow/TableList.jsx
+++ src/components/Operation/TwoTickets/OperateFlow/TableList.jsx
@@ -254,7 +254,7 @@ class TableList extends Component {
                 textAlign: 'center',
                 className: styles.picture,
                 render: (text, record) => (
-                    <i className="iconfont icon-todo" onClick={() => { this.showImgs(record); }} />
+                    <i className={`iconfont icon-todo ${styles.pictureIcon}`} onClick={() => { this.showImgs(record); }} />
                 ),
             },
             {
@@ -264,7 +264,7 @@ class TableList extends Component {
                 textAlign: 'center',
                 className: styles.opreate,
                 render: (text, record) => (
-                    <div>
+                    <div className={styles.opreateIcon}>
                         <i className={`iconfont icon-look ${styles.lookIcon}`} onClick={() => { this.onShowDetail(record); }} />
                         <i className={`iconfont icon-del ${(handleRight('operationTicket_operate') && record.stateCode === '2') ? styles.iconShow : styles.iconHide}`} onClick={() => { this.delList('del', record.docketId); }} />
                     </div>
