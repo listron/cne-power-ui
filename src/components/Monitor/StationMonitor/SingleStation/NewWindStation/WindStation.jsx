3e845e476479964605a5769f51cea81b05eb0e02
haojianling <591191280@qq.com>
2020-05-01 22:11:27 +0800

    图标修改

--- src/components/Monitor/StationMonitor/SingleStation/NewWindStation/WindStation.jsx
+++ src/components/Monitor/StationMonitor/SingleStation/NewWindStation/WindStation.jsx
@@ -62,7 +62,7 @@ class WindStation extends Component {
     this.state = {
       hiddenStationList: false,
       singleDeviceType: 0,
-    }
+    };
   }
 
   componentDidMount() {
@@ -73,12 +73,12 @@ class WindStation extends Component {
     this.getPowerDataTenMin({ stationCode, stationType }); // 发电量
     const main = document.getElementById('main');
     main.scrollTo(0, 0);
-    this.props.changeSingleStationStore({ deviceTypeCode: '101' })
+    this.props.changeSingleStationStore({ deviceTypeCode: '101' });
     this.props.getSingleRealChartsData({ // 1小时数据 出力图 等效利用小时
       stationCode,
       stationType,
       startTime: moment().subtract(24, 'hours').utc().format(),
-      endTime: moment().utc().format()
+      endTime: moment().utc().format(),
     });
   }
 
@@ -91,7 +91,7 @@ class WindStation extends Component {
       clearTimeout(this.timeOutId);
       this.props.resetSingleStationStore();
       this.getTenSeconds(nextStationCode, nextStationType);
-      this.props.changeSingleStationStore({ deviceTypeCode: '101' })
+      this.props.changeSingleStationStore({ deviceTypeCode: '101' });
       this.getPowerDataTenMin({ stationCode: nextStationCode, stationType: nextStationType });
       this.props.getNewDeviceTypeFlow({ stationCode: nextStationCode, stationType: nextStationType });//获取设备类型流程图
       this.props.stopSingleRealData();
@@ -99,7 +99,7 @@ class WindStation extends Component {
         stationCode: nextStationCode,
         stationType: nextStationType,
         startTime: moment().subtract(24, 'hours').utc().format(),
-        endTime: moment().utc().format()
+        endTime: moment().utc().format(),
       });
     }
   }
@@ -113,13 +113,12 @@ class WindStation extends Component {
 
   onHandleStation = (e) => {
     this.setState({
-      singleDeviceType: e,
-    })
+      singleDeviceType: e.target.value,
+    });
   }
 
   onSelectedDeviceType = (e) => { // 选择设备
-    const deviceTypeCode = parseInt(e.target.value);
-    this.props.changeSingleStationStore({ deviceTypeCode });
+    this.props.changeSingleStationStore({ deviceTypeCode: +(e.target.value) });
   }
 
 
@@ -127,8 +126,8 @@ class WindStation extends Component {
     this.props.getWindSingleStation({ stationCode });
     this.props.getAlarmList({ stationCode });
     this.props.getWeatherList({ stationCode }); // 天气
-    let endTime = moment().utc().format();
-    this.props.getWorkList({ stationCode, startTime: moment().set({ 'hour': 0, 'minute': 0, 'second': 0, }).utc().format(), endTime, });
+    const endTime = moment().utc().format();
+    this.props.getWorkList({ stationCode, startTime: moment().set({ 'hour': 0, 'minute': 0, 'second': 0 }).utc().format(), endTime });
     this.timeOutId = setTimeout(() => {
       this.getTenSeconds(stationCode, stationType);
     }, 10000);
@@ -137,8 +136,8 @@ class WindStation extends Component {
 
   getStatusNum = (status) => { // 获取状态的数量
     const { deviceStatusSummary = [] } = this.props.fanList;
-    const statusList = deviceStatusSummary.filter(e => e.deviceStatusCode === status)
-    return statusList.length > 0 && statusList[0].deviceStatusNum || 0
+    const statusList = deviceStatusSummary.filter(e => e.deviceStatusCode === status);
+    return statusList.length > 0 && statusList[0].deviceStatusNum || 0;
   }
 
 
@@ -147,12 +146,12 @@ class WindStation extends Component {
     const { stationCode } = this.props.match.params;
     const { intervalTime = 0 } = value;
     const stationType = '0';
-    let startTime = moment().subtract(6, 'day').format('YYYY-MM-DD')// 默认是6天前;
+    let startTime = moment().subtract(6, 'day').format('YYYY-MM-DD');// 默认是6天前;
     if (intervalTime === 1) {
-      startTime = moment().subtract(5, 'month').startOf('month').format('YYYY-MM-DD')
+      startTime = moment().subtract(5, 'month').startOf('month').format('YYYY-MM-DD');
     }
     if (intervalTime === 2) {
-      startTime = moment().subtract(5, 'year').startOf('year').format('YYYY-MM-DD')
+      startTime = moment().subtract(5, 'year').startOf('year').format('YYYY-MM-DD');
     }
     this.props.changeSingleStationStore({ windPowerData: [] });
     this.props.getWindMonitorPower({ // 出力图数据
@@ -174,24 +173,23 @@ class WindStation extends Component {
         list.push({
           deviceTypeCode: e.code,
           deviceTypeName: e.name,
-        })
+        });
         if (e.parents) {
-          this.getDeviceTypeFlow(e.parents, list)
+          this.getDeviceTypeFlow(e.parents, list);
         }
       }
-    })
-    return list
+    });
+    return list;
   }
 
 
   createFlowButton = (typeCode, typeName, buttonClass, imgClass, clickable = true, alarm = false) => ( // 设备流程生成函数
-    <RadioButton value={typeCode} className={styles[buttonClass]} style={clickable ? null : { pointerEvents: 'none' }} key={Math.random()}>
+    <RadioButton value={typeCode} className={styles[buttonClass]} style={clickable ? null : { pointerEvents: 'none' }} key={Math.random()} >
       <div className={styles.deviceTypeIcon} >
         <i className={getDeviceTypeIcon(+typeCode)} ></i>
         {alarm && <i className="iconfont icon-alarm alarmIcon" ></i>}
         {/* <img src="/img/arrowgo.png" className={styles[imgClass]} /> */}
       </div>
-      {/* <div>{typeName}</div> */}
     </RadioButton>
   )
 
@@ -200,17 +198,17 @@ class WindStation extends Component {
 
 
   render() {
-    const { deviceTypeFlow, deviceTypeCode, singleStationData, fanDisplay, windPowerData, singleStationScatter = {}, windCapabilityData, editData, } = this.props;
+    const { deviceTypeFlow, deviceTypeCode, singleStationData, fanDisplay, windPowerData, singleStationScatter = {}, windCapabilityData, editData } = this.props;
     const { windCapabilityDataTime, singleStationScattertime, windPowerTime } = this.props;
     const { singleStationScatterLoading = false, windPowerLoading = false, capabilityDataLoading = false } = this.props;
     const { stationCode } = this.props.match.params;
     const { singleDeviceType } = this.state;
     const deviceType = this.getDeviceTypeFlow([deviceTypeFlow]);
     const alarmList = this.props[getAlarmStatus(deviceTypeCode)];
-    let alarmStatus = alarmList ? !(alarmList instanceof Array) && alarmList.deviceList && alarmList.deviceList.some(e => e.alarmNum > 0) || (alarmList.length > 0 && alarmList.some(e => e.warningStatus)) : false;
+    const alarmStatus = alarmList ? !(alarmList instanceof Array) && alarmList.deviceList && alarmList.deviceList.some(e => e.alarmNum > 0) || (alarmList.length > 0 && alarmList.some(e => e.warningStatus)) : false;
 
     const stautus = [
-      { text: '运行', deviceStatusCode: 400, },
+      { text: '运行', deviceStatusCode: 400 },
       { text: '待机', deviceStatusCode: 700 },
       { text: '停机', deviceStatusCode: 200 },
       { text: '维护', deviceStatusCode: 600 },
@@ -232,30 +230,22 @@ class WindStation extends Component {
           <div className={styles.windList}>
             <div className={styles.threadAndDevice} id="deviceType" >
               <div className={styles.deviceTypeFlow}>
-                <RadioGroup value={deviceTypeCode} onChange={this.onSelectedDeviceType} >
+                <RadioGroup value={`${deviceTypeCode}`} onChange={this.onSelectedDeviceType} >
                   {deviceType.map((item, index) => {
-                    return (this.createFlowButton(item.deviceTypeCode, item.deviceTypeName, 'deviceTypeItem', 'arrowgo', true, deviceTypeCode === item.deviceTypeCode && alarmStatus))
+                    return (this.createFlowButton(item.deviceTypeCode, item.deviceTypeName, 'deviceTypeItem', 'arrowgo', true, deviceTypeCode === item.deviceTypeCode && alarmStatus));
                   })}
-                  <RadioButton value={0} className={styles.elecnettingItem} key={'0'}>
+                  <RadioButton value={'0'} className={styles.elecnettingItem} key={'0'}>
                     <div className={styles.deviceTypeIcon} >
                       <i className="iconfont icon-elecnetting" ></i>
                     </div>
-                    {/* <div>电网</div> */}
                   </RadioButton>
                 </RadioGroup>
               </div>
               {deviceTypeCode === '101' &&
-                <div className={styles.singleDeviceTypeBox}>
-                  <span onClick={() => { this.onHandleStation(0) }}
-                    className={singleDeviceType === 0 ? styles.spanActive : styles.spanNormal} > 全部</span>
-                  {stautus.map(e => {
-                    return (<span key={e.deviceStatusCode}
-                      onClick={() => { this.onHandleStation(e.deviceStatusCode) }}
-                      className={singleDeviceType === e.deviceStatusCode ? styles.spanActive : styles.spanNormal}>
-                      {e.text} {this.getStatusNum(e.deviceStatusCode)}
-                    </span>)
-                  })}
-                </div>
+                <RadioGroup onChange={this.onHandleStation} value={singleDeviceType} className={styles.singleDeviceTypeBox}>
+                  <RadioButton key={0} value={0} >全部</RadioButton>
+                  {stautus.map(e => (<RadioButton key={e.deviceStatusCode} value={e.deviceStatusCode}> {e.text} {this.getStatusNum(e.deviceStatusCode)}</RadioButton>))}
+                </RadioGroup>
               }
             </div>
             <div>
@@ -269,8 +259,8 @@ class WindStation extends Component {
             <div className={styles.windStationChart}>
               <div className={styles.tags}>
                 <CneButton lengthMode="long"><Link to={`/monitor/alarm/realtime?stationCode=${stationCode}`} className={styles.alarmBtn}> 查看告警 {dataFormats(singleStationData.alarmNum, '--')} </Link></CneButton>
-                <CneButton disabled lengthMode="short"><Link to={`javascript:void(0)`} className={styles.noLink}> 统计分析  </Link></CneButton>
-                <CneButton lengthMode="short"><Link to={`/report/windstation/powerReport`} className={styles.reportBtn}> 报表查询  </Link></CneButton>
+                <CneButton disabled lengthMode="short"><Link to={'javascript:void(0)'} className={styles.noLink}> 统计分析  </Link></CneButton>
+                <CneButton lengthMode="short"><Link to={'/report/windstation/powerReport'} className={styles.reportBtn}> 报表查询  </Link></CneButton>
               </div>
               <div className={styles.chartsBox}>
                 <OutputChart
@@ -299,7 +289,7 @@ class WindStation extends Component {
           }
         </div>
       </div>
-    )
+    );
   }
 }
 
